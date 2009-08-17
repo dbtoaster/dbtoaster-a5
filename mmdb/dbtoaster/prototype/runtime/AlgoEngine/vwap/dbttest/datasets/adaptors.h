b90b678acfa781ca5fefd9b5202d27de7155331e
Anton Morozov <amorozov@dbtoaster.org>
2009-08-17 23:16:24 +0000

    fixed datasets for AlgoEngine
    Code for AlgoEngine
    4 different buy/sell algos
    Server for GUI connection
    toaster and server clients
    
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@265 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 5cb03297881f70661e8aee45dbc16a26542164b9

---
+++ mmdb/dbtoaster/prototype/runtime/AlgoEngine/vwap/dbttest/datasets/adaptors.h
@@ -0,0 +1,166 @@
+#ifndef DBTOASTER_ADAPTORS_H
+#define DBTOASTER_ADAPTORS_H
+
+#include <map>
+#include "datasets.h"
+#include "streamengine.h"
+
+namespace DBToaster {
+
+    namespace Adaptors
+    {
+        using namespace DBToaster::StandaloneEngine;
+
+        template <typename ResultType>
+        struct Adaptor
+        {
+            typedef ResultType Result;
+            virtual void operator()(DBToasterTuple&, boost::any&) = 0;
+        };
+
+
+        template<typename ResultType>
+        struct InsertTupleAdaptor : public Adaptor<ResultType>
+        {
+            InsertTupleAdaptor(){}
+            void operator()(DBToasterTuple& a, boost::any& b)
+            {
+                a.type = DBToaster::StandaloneEngine::insertTuple;
+                a.data = b;
+            }
+        };
+
+        template<typename ResultType>
+        struct DeleteTupleAdaptor : public Adaptor<ResultType>
+        {
+            DeleteTupleAdaptor() {}
+            void operator()(DBToasterTuple& a, boost::any& b)
+            {
+                a.type = DBToaster::StandaloneEngine::deleteTuple;
+                a.data = b;
+            }
+        };
+    };
+
+    namespace DemoDatasets
+    {
+        using namespace std;
+        using namespace DBToaster::StandaloneEngine;
+        using namespace DBToaster::Adaptors;
+
+        typedef struct {
+            int t;
+            int id;
+            int price;
+            int volume;
+        } VwapHandlerInput;
+
+        struct VwapTupleAdaptor : public Adaptor<VwapHandlerInput>
+        {
+            typedef map<int, tuple<double, double> > order_ids;
+
+            order_ids bid_orders;
+            order_ids ask_orders;
+
+            VwapTupleAdaptor() {}
+
+            void operator()(DBToasterTuple& a, boost::any& b)
+            {
+                VwapTuple* v = any_cast<VwapTuple>(&b);
+
+                if ( v->action == "B" ) {
+                    bid_orders[v->id] = make_tuple(v->price, v->volume);
+                    a.type = DBToaster::StandaloneEngine::insertTuple;
+                }
+
+                else if ( v->action == "S" ) {
+                    ask_orders[v->id] = make_tuple(v->price, v->volume);
+                    a.type = DBToaster::StandaloneEngine::insertTuple;
+                }
+
+                else if ( v->action == "E" )
+                    // This is an update, how do generate an additional insert?
+                    a.type = DBToaster::StandaloneEngine::deleteTuple;
+
+                else if ( v->action == "F" )
+                {
+                    order_ids::iterator bid_found = bid_orders.find(v->id);
+                    if ( bid_found != bid_orders.end() )
+                    {
+                        v->price = get<0>(bid_found->second);
+                        v->volume = get<1>(bid_found->second);
+                        bid_orders.erase(bid_found);
+                    }
+                    else
+                    {
+                        order_ids::iterator ask_found = ask_orders.find(v->id);
+                        if ( ask_found != ask_orders.end() )
+                        {
+                            v->price = get<0>(ask_found->second);
+                            v->volume = get<1>(ask_found->second);
+                            ask_orders.erase(ask_found);
+                        }
+                        // TODO: handle invalid tuples that are neither bids nor sell...
+                    }
+
+                    a.type = DBToaster::StandaloneEngine::deleteTuple;
+                }
+
+                else if ( v->action == "D" )
+                {
+                    order_ids::iterator bid_found = bid_orders.find(v->id);
+                    if ( bid_found != bid_orders.end() )
+                    {
+                        v->price = get<0>(bid_found->second);
+                        v->volume = get<1>(bid_found->second);
+                        bid_orders.erase(bid_found);
+                    }
+                    else
+                    {
+                        order_ids::iterator ask_found = ask_orders.find(v->id);
+                        if ( ask_found != ask_orders.end() )
+                        {
+                            v->price = get<0>(ask_found->second);
+                            v->volume = get<1>(ask_found->second);
+                            ask_orders.erase(ask_found);
+                        }
+                        // TODO: handle invalid tuples that are neither bids nor sell...
+                    }
+
+                    a.type = DBToaster::StandaloneEngine::deleteTuple;
+                }
+
+                /*
+                // ignore for now...
+                else if ( v->action == "X")
+                else if ( v->action == "C")
+                else if ( v->action == "T")
+                */
+ 
+                // TODO: casting from doubles to ints
+                VwapHandlerInput r;
+                r.t = v->t;
+                r.id = v->id;
+                r.price = v->price;
+                r.volume = v->volume;
+                a.data = r;
+            }
+        };
+
+        struct SsbTupleAdaptor
+        {
+            SsbTupleAdaptor() {}
+            void operator()(DBToasterTuple& a, boost::any& b)
+            {}
+        };
+
+        struct LinearRoadTupleAdaptor
+        {
+            LinearRoadTupleAdaptor() {}
+            void operator()(DBToasterTuple& a, boost::any& b)
+            {}
+        };
+    };
+};
+    
+#endif
