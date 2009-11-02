98a3c17ffa7a4883175863aabe7afba48beaf3c8
Yanif Ahmad <yanif.ahmad@gmail.com>
2009-11-02 11:55:58 +0000

    Naive versions of queries.
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@528 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 2954c962895f8a8d9da68446e097131a99c47de1

---
+++ mmdb/dbtoaster/prototype/compiler/alpha3/experiments/naive/tpch/query22/query22.cc
@@ -0,0 +1,338 @@
+// DBToaster includes.
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+
+#include <iostream>
+#include <map>
+#include <list>
+#include <set>
+
+#include <tr1/cstdint>
+#include <tr1/tuple>
+#include <tr1/unordered_set>
+
+using namespace std;
+using namespace tr1;
+
+
+
+// Stream engine includes.
+#include "standalone/streamengine.h"
+#include "profiler/profiler.h"
+#include "datasets/adaptors.h"
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/pool/pool.hpp>
+#include <boost/pool/pool_alloc.hpp>
+
+
+using namespace DBToaster::Profiler;
+map<int64_t,double> q;
+multiset<tuple<int64_t,int64_t,string,double,string,string,string,int,string> > ORDERS;
+multiset<tuple<int64_t,string,string,int64_t,string,double,string,string> > CUSTOMER;
+
+double on_insert_CUSTOMER_sec_span = 0.0;
+double on_insert_CUSTOMER_usec_span = 0.0;
+double on_insert_ORDERS_sec_span = 0.0;
+double on_insert_ORDERS_usec_span = 0.0;
+double on_delete_CUSTOMER_sec_span = 0.0;
+double on_delete_CUSTOMER_usec_span = 0.0;
+double on_delete_ORDERS_sec_span = 0.0;
+double on_delete_ORDERS_usec_span = 0.0;
+
+
+
+void analyse_mem_usage(ofstream* stats)
+{
+   cout << "ORDERS size: " << (((sizeof(multiset<tuple<int64_t,int64_t,string,double,string,string,string,int,string> >::value_type)
+       + sizeof(struct _Rb_tree_node_base))
+       * ORDERS.size())  + (sizeof(struct _Rb_tree<multiset<tuple<int64_t,int64_t,string,double,string,string,string,int,string> >::key_type, multiset<tuple<int64_t,int64_t,string,double,string,string,string,int,string> >::value_type, _Identity<multiset<tuple<int64_t,int64_t,string,double,string,string,string,int,string> >::value_type>, multiset<tuple<int64_t,int64_t,string,double,string,string,string,int,string> >::key_compare>))) << endl;
+
+   (*stats) << "m," << "ORDERS" << "," << (((sizeof(multiset<tuple<int64_t,int64_t,string,double,string,string,string,int,string> >::value_type)
+       + sizeof(struct _Rb_tree_node_base))
+       * ORDERS.size())  + (sizeof(struct _Rb_tree<multiset<tuple<int64_t,int64_t,string,double,string,string,string,int,string> >::key_type, multiset<tuple<int64_t,int64_t,string,double,string,string,string,int,string> >::value_type, _Identity<multiset<tuple<int64_t,int64_t,string,double,string,string,string,int,string> >::value_type>, multiset<tuple<int64_t,int64_t,string,double,string,string,string,int,string> >::key_compare>))) << endl;
+
+   cout << "q size: " << (((sizeof(map<int64_t,double>::key_type)
+       + sizeof(map<int64_t,double>::mapped_type)
+       + sizeof(struct _Rb_tree_node_base))
+       * q.size())  + (sizeof(struct _Rb_tree<map<int64_t,double>::key_type, map<int64_t,double>::value_type, _Select1st<map<int64_t,double>::value_type>, map<int64_t,double>::key_compare>))) << endl;
+
+   (*stats) << "m," << "q" << "," << (((sizeof(map<int64_t,double>::key_type)
+       + sizeof(map<int64_t,double>::mapped_type)
+       + sizeof(struct _Rb_tree_node_base))
+       * q.size())  + (sizeof(struct _Rb_tree<map<int64_t,double>::key_type, map<int64_t,double>::value_type, _Select1st<map<int64_t,double>::value_type>, map<int64_t,double>::key_compare>))) << endl;
+
+   cout << "CUSTOMER size: " << (((sizeof(multiset<tuple<int64_t,string,string,int64_t,string,double,string,string> >::value_type)
+       + sizeof(struct _Rb_tree_node_base))
+       * CUSTOMER.size())  + (sizeof(struct _Rb_tree<multiset<tuple<int64_t,string,string,int64_t,string,double,string,string> >::key_type, multiset<tuple<int64_t,string,string,int64_t,string,double,string,string> >::value_type, _Identity<multiset<tuple<int64_t,string,string,int64_t,string,double,string,string> >::value_type>, multiset<tuple<int64_t,string,string,int64_t,string,double,string,string> >::key_compare>))) << endl;
+
+   (*stats) << "m," << "CUSTOMER" << "," << (((sizeof(multiset<tuple<int64_t,string,string,int64_t,string,double,string,string> >::value_type)
+       + sizeof(struct _Rb_tree_node_base))
+       * CUSTOMER.size())  + (sizeof(struct _Rb_tree<multiset<tuple<int64_t,string,string,int64_t,string,double,string,string> >::key_type, multiset<tuple<int64_t,string,string,int64_t,string,double,string,string> >::value_type, _Identity<multiset<tuple<int64_t,string,string,int64_t,string,double,string,string> >::value_type>, multiset<tuple<int64_t,string,string,int64_t,string,double,string,string> >::key_compare>))) << endl;
+
+}
+
+void analyse_handler_usage(ofstream* stats)
+{
+   cout << "on_insert_CUSTOMER cost: " << (on_insert_CUSTOMER_sec_span + (on_insert_CUSTOMER_usec_span / 1000000.0)) << endl;
+   (*stats) << "h," << "on_insert_CUSTOMER" << "," << (on_insert_CUSTOMER_sec_span + (on_insert_CUSTOMER_usec_span / 1000000.0)) << endl;
+   cout << "on_insert_ORDERS cost: " << (on_insert_ORDERS_sec_span + (on_insert_ORDERS_usec_span / 1000000.0)) << endl;
+   (*stats) << "h," << "on_insert_ORDERS" << "," << (on_insert_ORDERS_sec_span + (on_insert_ORDERS_usec_span / 1000000.0)) << endl;
+   cout << "on_delete_CUSTOMER cost: " << (on_delete_CUSTOMER_sec_span + (on_delete_CUSTOMER_usec_span / 1000000.0)) << endl;
+   (*stats) << "h," << "on_delete_CUSTOMER" << "," << (on_delete_CUSTOMER_sec_span + (on_delete_CUSTOMER_usec_span / 1000000.0)) << endl;
+   cout << "on_delete_ORDERS cost: " << (on_delete_ORDERS_sec_span + (on_delete_ORDERS_usec_span / 1000000.0)) << endl;
+   (*stats) << "h," << "on_delete_ORDERS" << "," << (on_delete_ORDERS_sec_span + (on_delete_ORDERS_usec_span / 1000000.0)) << endl;
+}
+
+
+////////////////////////////////////////
+//
+// Query plan
+
+void recompute()
+{
+    map<int64_t,double>::iterator q_it8 = q.begin();
+    map<int64_t,double>::iterator q_end7 = q.end();
+    for (; q_it8 != q_end7; ++q_it8)
+    {
+        int64_t NATIONKEY = q_it8->first;
+        q[NATIONKEY] = 0;
+        
+        multiset<tuple<int64_t,string,string,int64_t,string,double,string,string> 
+            >::iterator CUSTOMER_it6 = CUSTOMER.begin();
+        
+        multiset<tuple<int64_t,string,string,int64_t,string,double,string,string> 
+            >::iterator CUSTOMER_end5 = CUSTOMER.end();
+        for (; CUSTOMER_it6 != CUSTOMER_end5; ++CUSTOMER_it6)
+        {
+            int64_t protect_C1__CUSTKEY = get<0>(*CUSTOMER_it6);
+            int64_t protect_C1__NATIONKEY = get<3>(*CUSTOMER_it6);
+            double protect_C1__ACCTBAL = get<5>(*CUSTOMER_it6);
+
+            int var8 = 0;
+            
+            multiset<tuple<int64_t,int64_t,string,double,string,string,string,int,string> 
+                >::iterator ORDERS_it4 = ORDERS.begin();
+            
+            multiset<tuple<int64_t,int64_t,string,double,string,string,string,int,string> 
+                >::iterator ORDERS_end3 = ORDERS.end();
+            for (; ORDERS_it4 != ORDERS_end3; ++ORDERS_it4)
+            {
+                int64_t protect_O__CUSTKEY = get<1>(*ORDERS_it4);
+                if ( protect_O__CUSTKEY == protect_C1__CUSTKEY )
+                    var8 += 1;
+            }
+
+            if ( 0 == var8 )
+            {
+                double var4 = 0;
+                
+                multiset<tuple<int64_t,string,string,int64_t,string,double,string,string> 
+                    >::iterator CUSTOMER_it2 = CUSTOMER.begin();
+                
+                multiset<tuple<int64_t,string,string,int64_t,string,double,string,string> 
+                    >::iterator CUSTOMER_end1 = CUSTOMER.end();
+                for (; CUSTOMER_it2 != CUSTOMER_end1; ++CUSTOMER_it2)
+                {
+                    double C2__ACCTBAL = get<5>(*CUSTOMER_it2);
+                    if ( 0 < C2__ACCTBAL )
+                        var4 += C2__ACCTBAL;
+                }
+
+                if ( protect_C1__ACCTBAL < var4
+                     && NATIONKEY == protect_C1__NATIONKEY )
+                {
+                    q[NATIONKEY] += protect_C1__ACCTBAL;
+                }
+            }
+        }
+    }
+}
+
+////////////////////////////////////////
+//
+// Triggers
+
+void on_insert_CUSTOMER(
+    ofstream* results, ofstream* log, ofstream* stats, int64_t CUSTKEY,string 
+    NAME,string ADDRESS,int64_t NATIONKEY,string PHONE,double ACCTBAL,string 
+    MKTSEGMENT,string COMMENT)
+{
+    struct timeval hstart, hend;
+    gettimeofday(&hstart, NULL);
+    CUSTOMER.insert(make_tuple(
+        CUSTKEY,NAME,ADDRESS,NATIONKEY,PHONE,ACCTBAL,MKTSEGMENT,COMMENT));
+
+    recompute();
+
+    gettimeofday(&hend, NULL);
+    DBToaster::Profiler::accumulate_time_span(
+        hstart, hend, on_insert_CUSTOMER_sec_span, on_insert_CUSTOMER_usec_span);
+}
+
+void on_insert_ORDERS(
+    ofstream* results, ofstream* log, ofstream* stats, int64_t ORDERKEY,int64_t 
+    CUSTKEY,string ORDERSTATUS,double TOTALPRICE,string ORDERDATE,string 
+    ORDERPRIORITY,string CLERK,int SHIPPRIORITY,string COMMENT)
+{
+    struct timeval hstart, hend;
+    gettimeofday(&hstart, NULL);
+    ORDERS.insert(make_tuple(
+        ORDERKEY,CUSTKEY,ORDERSTATUS,TOTALPRICE,ORDERDATE,ORDERPRIORITY,CLERK,
+        SHIPPRIORITY,COMMENT));
+
+    recompute();
+
+    gettimeofday(&hend, NULL);
+    DBToaster::Profiler::accumulate_time_span(
+        hstart, hend, on_insert_ORDERS_sec_span, on_insert_ORDERS_usec_span);
+}
+
+void on_delete_CUSTOMER(
+    ofstream* results, ofstream* log, ofstream* stats, int64_t CUSTKEY,string 
+    NAME,string ADDRESS,int64_t NATIONKEY,string PHONE,double ACCTBAL,string 
+    MKTSEGMENT,string COMMENT)
+{
+    struct timeval hstart, hend;
+    gettimeofday(&hstart, NULL);
+    CUSTOMER.erase(make_tuple(
+        CUSTKEY,NAME,ADDRESS,NATIONKEY,PHONE,ACCTBAL,MKTSEGMENT,COMMENT));
+
+    recompute();
+
+    gettimeofday(&hend, NULL);
+    DBToaster::Profiler::accumulate_time_span(
+        hstart, hend, on_delete_CUSTOMER_sec_span, on_delete_CUSTOMER_usec_span);
+}
+
+void on_delete_ORDERS(
+    ofstream* results, ofstream* log, ofstream* stats, int64_t ORDERKEY,int64_t 
+    CUSTKEY,string ORDERSTATUS,double TOTALPRICE,string ORDERDATE,string 
+    ORDERPRIORITY,string CLERK,int SHIPPRIORITY,string COMMENT)
+{
+    struct timeval hstart, hend;
+    gettimeofday(&hstart, NULL);
+    ORDERS.erase(make_tuple(
+        ORDERKEY,CUSTKEY,ORDERSTATUS,TOTALPRICE,ORDERDATE,ORDERPRIORITY,CLERK,
+        SHIPPRIORITY,COMMENT));
+
+    recompute();
+
+    gettimeofday(&hend, NULL);
+    DBToaster::Profiler::accumulate_time_span(
+        hstart, hend, on_delete_ORDERS_sec_span, on_delete_ORDERS_usec_span);
+}
+
+DBToaster::DemoDatasets::CustomerStream SSBCustomer("/Users/yanif/datasets/tpch/sf1/singlefile/customer.tbl.a",&DBToaster::DemoDatasets::parseCustomerField,8,1600000, 512);
+
+boost::shared_ptr<DBToaster::DemoDatasets::CustomerTupleAdaptor> SSBCustomer_adaptor(new DBToaster::DemoDatasets::CustomerTupleAdaptor());
+static int streamSSBCustomerId = 0;
+
+struct on_insert_CUSTOMER_fun_obj { 
+    void operator()(boost::any data, ofstream* results, ofstream* log, ofstream* stats) { 
+        DBToaster::DemoDatasets::CustomerTupleAdaptor::Result input = 
+            boost::any_cast<DBToaster::DemoDatasets::CustomerTupleAdaptor::Result>(data); 
+        on_insert_CUSTOMER(results, log, stats, input.custkey,input.name,input.address,input.nationkey,input.phone,input.acctbal,input.mktsegment,input.comment);
+    }
+};
+
+on_insert_CUSTOMER_fun_obj fo_on_insert_CUSTOMER_0;
+
+DBToaster::DemoDatasets::OrderStream SSBOrder("/Users/yanif/datasets/tpch/sf1/singlefile/orders.tbl.a",&DBToaster::DemoDatasets::parseOrderField,9,17000000,512);
+
+boost::shared_ptr<DBToaster::DemoDatasets::OrderTupleAdaptor> SSBOrder_adaptor(new DBToaster::DemoDatasets::OrderTupleAdaptor());
+static int streamSSBOrderId = 1;
+
+struct on_insert_ORDERS_fun_obj { 
+    void operator()(boost::any data, ofstream* results, ofstream* log, ofstream* stats) { 
+        DBToaster::DemoDatasets::OrderTupleAdaptor::Result input = 
+            boost::any_cast<DBToaster::DemoDatasets::OrderTupleAdaptor::Result>(data); 
+        on_insert_ORDERS(results, log, stats, input.orderkey,input.custkey,input.orderstatus,input.totalprice,input.orderdate,input.orderpriority,input.clerk,input.shippriority,input.comment);
+    }
+};
+
+on_insert_ORDERS_fun_obj fo_on_insert_ORDERS_1;
+
+struct on_delete_CUSTOMER_fun_obj { 
+    void operator()(boost::any data, ofstream* results, ofstream* log, ofstream* stats) { 
+        DBToaster::DemoDatasets::CustomerTupleAdaptor::Result input = 
+            boost::any_cast<DBToaster::DemoDatasets::CustomerTupleAdaptor::Result>(data); 
+        on_delete_CUSTOMER(results, log, stats, input.custkey,input.name,input.address,input.nationkey,input.phone,input.acctbal,input.mktsegment,input.comment);
+    }
+};
+
+on_delete_CUSTOMER_fun_obj fo_on_delete_CUSTOMER_2;
+
+struct on_delete_ORDERS_fun_obj { 
+    void operator()(boost::any data, ofstream* results, ofstream* log, ofstream* stats) { 
+        DBToaster::DemoDatasets::OrderTupleAdaptor::Result input = 
+            boost::any_cast<DBToaster::DemoDatasets::OrderTupleAdaptor::Result>(data); 
+        on_delete_ORDERS(results, log, stats, input.orderkey,input.custkey,input.orderstatus,input.totalprice,input.orderdate,input.orderpriority,input.clerk,input.shippriority,input.comment);
+    }
+};
+
+on_delete_ORDERS_fun_obj fo_on_delete_ORDERS_3;
+
+
+void init(DBToaster::StandaloneEngine::FileMultiplexer& sources,
+    DBToaster::StandaloneEngine::FileStreamDispatcher& router, 
+    ofstream* results, ofstream* log, ofstream* stats)
+{
+    router.setFiles(results, log, stats);
+    sources.addStream<DBToaster::DemoDatasets::customer>(&SSBCustomer, boost::ref(*SSBCustomer_adaptor), streamSSBCustomerId);
+    router.addHandler(streamSSBCustomerId,DBToaster::StandaloneEngine::insertTuple,boost::ref(fo_on_insert_CUSTOMER_0));
+    sources.addStream<DBToaster::DemoDatasets::order>(&SSBOrder, boost::ref(*SSBOrder_adaptor), streamSSBOrderId);
+    router.addHandler(streamSSBOrderId,DBToaster::StandaloneEngine::insertTuple,boost::ref(fo_on_insert_ORDERS_1));
+    router.addHandler(streamSSBCustomerId,DBToaster::StandaloneEngine::deleteTuple,boost::ref(fo_on_delete_CUSTOMER_2));
+    router.addHandler(streamSSBOrderId,DBToaster::StandaloneEngine::deleteTuple,boost::ref(fo_on_delete_ORDERS_3));
+    sources.initStream();
+    cout << "Initialized input stream..." << endl;
+}
+
+DBToaster::StandaloneEngine::FileMultiplexer sources(12345, 20);
+DBToaster::StandaloneEngine::FileStreamDispatcher router;
+
+
+void runMultiplexer(ofstream* results, ofstream* log, ofstream* stats)
+{
+    unsigned long tuple_counter = 0;
+    double tup_sec_span = 0.0;
+    double tup_usec_span = 0.0;
+    struct timeval tvs, tve;
+    gettimeofday(&tvs, NULL);
+    while ( sources.streamHasInputs() ) {
+        struct timeval tups, tupe;
+        gettimeofday(&tups, NULL);
+        DBToaster::StandaloneEngine::DBToasterTuple t = sources.nextInput();
+        router.dispatch(t);
+        ++tuple_counter;
+        gettimeofday(&tupe, NULL);
+        DBToaster::Profiler::accumulate_time_span(tups, tupe, tup_sec_span, tup_usec_span);
+        if ( (tuple_counter % 10000) == 0 )
+        {
+            DBToaster::Profiler::reset_time_span_printing_global(
+                "tuples", tuple_counter, 10000, tvs, tup_sec_span, tup_usec_span, "query", log);
+            analyse_mem_usage(stats);
+            analyse_handler_usage(stats);
+        }
+    }
+    DBToaster::Profiler::reset_time_span_printing_global(
+        "tuples", tuple_counter, (tuple_counter%10000), tvs, tup_sec_span, tup_usec_span, "query", log);
+    analyse_handler_usage(stats);
+    analyse_mem_usage(stats);
+}
+
+
+int main(int argc, char* argv[])
+{
+    DBToaster::StandaloneEngine::EngineOptions opts;
+    opts(argc,argv);
+    ofstream* log = new ofstream(opts.log_file_name.c_str());
+    ofstream* results = new ofstream(opts.results_file_name.c_str());
+    ofstream* stats = new ofstream(opts.stats_file_name.c_str());
+    init(sources, router, results, log, stats);
+    runMultiplexer(results, log, stats);
+    log->flush(); log->close();
+    results->flush(); results->close();
+}
