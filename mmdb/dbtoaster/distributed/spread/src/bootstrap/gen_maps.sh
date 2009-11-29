6af214d4c3bccf4bf68edf2894d688e621be8745
Yanif Ahmad <yanif.ahmad@gmail.com>
2009-11-29 17:30:30 +0000

    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@649 e0e46c49-be69-4f5a-ad62-21024a331aea
    Former-commit-id: 3b8fd22b45f5967a67bec253b8ebbb5340f0f90c

---
+++ mmdb/dbtoaster/distributed/spread/src/bootstrap/gen_maps.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+dataset_dir=/home/yanif/datasets/tpch/
+output_dir=tpch/
+tmp_dir=maps/
+
+if [ $# -lt 1 ]; then
+    echo "Usage $0 [-d data dir] [-o output dir] [-t temp dir] <boot spec>"
+    exit
+fi
+
+while getopts ":d:o:" option
+do
+    case $option in
+      d)  dataset_dir=$OPTARG;;
+      o)  output_dir=$OPTARG;;
+      o)  tmp_dir=$OPTARG;;
+    esac
+done
+
+shift $(($OPTIND - 1))
+
+if [[ !( -d $dataset_dir && -d $output_dir && -d $tmp_dir ) ]]; then
+    echo "Invalid directories: $dataset_dir, $output_dir, $tmp_dir"
+    exit
+fi
+
+if [ ! -f $1 ]; then
+    echo "No such file: $1"
+    exit
+fi
+
+boot_file=$1
+mapnames=`awk '{ i=i+1; if (i%6 == 1) { names = names j $0; j = " " } } END { print names }' $boot_file`
+
+# Print task
+echo "Bootstrapping $boot_file"
+echo "Dataset: $dataset_dir"
+echo "Output dir: $output_dir"
+echo "Temp dir: $tmp_dir"
+echo "Maps: $mapnames"
+
+for i in 1g 10g; do
+
+    ./gen_ctl.sh $i
+    sqlplus / @tpch_boot.sql 
+    ./tpch_load.sh
+
+    rm -rf __db* maps/*
+    for n in 0 1 2 3 4 5 6 7; do
+        for m in `echo $mapnames`; do
+            time ./bootstrap.rb -d $dataset_dir/$i -n $n -m $m -o $tmp_dir $boot_file
+        done
+    done
+
+    test -d $output_dir/$i || (echo "Creating $output_dir/$i" && mkdir -p $output_dir/$i/)
+    mv __db* $output_dir/$i/
+    mv maps/* $output_dir/$i/
+done
