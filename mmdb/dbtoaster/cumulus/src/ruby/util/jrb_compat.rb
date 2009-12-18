5b66ed3fd374c42a22708899ad114be7fab0a5da
Oliver Kennedy <okennedy@buffalo.edu>
2009-12-18 07:39:39 +0000

    Missing files
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@680 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 618bad3fff3d9196935d38ffb77773fbdbe4ddb7

---
+++ mmdb/dbtoaster/cumulus/src/ruby/util/jrb_compat.rb
@@ -0,0 +1,10 @@
+
+include Java;
+include_class Java::org::dbtoaster::cumulus::net::SpreadException;
+include_class Java::org::dbtoaster::cumulus::net::NetTypes;
+
+MapEntry   = NetTypes::Entry;
+PutField   = NetTypes::PutField;
+PutParams  = NetTypes::PutParams;
+PutRequest = NetTypes::PutRequest;
+GetRequest = NetTypes::GetRequest;
