ef77d35238fbf6abfe636f6e286c94f78f3801e1
Oliver Kennedy <okennedy@buffalo.edu>
2009-12-20 00:12:25 +0000

    Properly deleting bin
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@703 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: c0a703c8f4a41dccafcc5cbc58981c579676a852

--- mmdb/dbtoaster/cumulus/bin/monitor.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-watch "ps auxww | grep ruby | grep -v grep | grep -v sed | sed 's/-I [^ ]* //g; s:ruby [^ ]*/\\([^/ \\-]*\\)-launcher.rb .*:\\1 :' | awk '{print \$11,\$3,\$2,\$5,\$6}' | column -t"
