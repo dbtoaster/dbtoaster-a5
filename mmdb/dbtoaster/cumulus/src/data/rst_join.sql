00eb0f3af7e06005980f8c0a308efe830bc71870
Oliver Kennedy <okennedy@buffalo.edu>
2010-01-07 01:30:37 +0000

    killing the fake data directory
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@745 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 3fac92a3c0f8adb5dfcacdf3730153704edd775a

--- mmdb/dbtoaster/cumulus/src/data/rst_join.sql
+++
@@ -1,8 +0,0 @@
---test T(6,65)
---test S(65,62)
---test S(606, 6)
---test R(4,606)
-create table r(a int, b int); 
-create table s(b int, c int); 
-create table t(c int, d int); 
-select sum(a*d) from r,s,t where r.b=s.b and s.c=t.c;
