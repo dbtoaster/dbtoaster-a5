c0d42397a7b5ebd9d46c8b2e908d3443b99a9dbb
Yanif Ahmad <yanif.ahmad@gmail.com>
2010-01-18 22:15:33 +0000

    Working multikeymap for HBase.
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@749 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 63f8268084a207261955acd65f985671132ce2ce

--- mmdb/dbtoaster/cumulus/src/ruby/bootstrap/multikeymap_HBase_JRB.rb
+++ mmdb/dbtoaster/cumulus/src/ruby/bootstrap/multikeymap_HBase_JRB.rb
@@ -1,10 +1,19 @@
+require 'singleton'
+
 include Java;
 
+import org.apache.hadoop.hbase.HBaseConfiguration
+import org.apache.hadoop.hbase.HColumnDescriptor
+import org.apache.hadoop.hbase.HTableDescriptor
 import org.apache.hadoop.hbase.client.HBaseAdmin
 import org.apache.hadoop.hbase.client.HTable
 import org.apache.hadoop.hbase.client.Get
 import org.apache.hadoop.hbase.client.Put
 import org.apache.hadoop.hbase.client.Scan
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.hbase.filter.FilterList
+import org.apache.hadoop.hbase.filter.CompareFilter
+import org.apache.hadoop.hbase.filter.SingleColumnValueFilter
 
 # HBase options for creating secondary indexes:
 # -- IndexedTable
@@ -15,8 +24,14 @@ import org.apache.hadoop.hbase.client.Scan
 # Ideally we would use a filter that can compare parts of a complex key, but
 # HBase doesn't seem to have one, and I don't want to write one for now...
 
+# Note we don't keep any state for patterns with this approach, i.e. secondary
+# indexes etc, so we can simply slice as needed, ad-hoc.
+
 class HBaseHandle
   include Singleton
+  
+  attr_reader :hbconf, :hbadmin
+
   def initialize()
     @hbconf = HBaseConfiguration.new()
     @hbadmin = HBaseAdmin.new(@hbconf)
@@ -26,50 +41,43 @@ end
 class MultiKeyMap 
 
   def initialize(numkeys, patterns, name = "", default = nil, wildcard = -1)
-    
     hbase_hndl = HBaseHandle.instance
     @numkeys = numkeys
     if not(hbase_hndl.hbadmin.tableExists(name)) then
-      htd = HTableDesriptor.new(name)
-      (0..@numkeys).each do |i|
-        htd.addFamily(HColumnDescriptor.new("key#{i.to_s}:"))
+      htd = HTableDescriptor.new(name)
+      (0...@numkeys).each do |i|
+        htd.addFamily(HColumnDescriptor.new(Bytes.toBytes("key#{i.to_s}")))
       end
-      htd.addFamily(HColumnDescriptor.new("value:"))
+      htd.addFamily(HColumnDescriptor.new(Bytes.toBytes("value")))
       hbase_hndl.hbadmin.createTable(htd)
     end
-    @htable = HTable.new(hbase_hndl.hbconf, name)
-    
-    @wildcard = wildcard
 
-    @patterns = []
-    patterns.each do |pattern|
-      @patterns << scand
-    end
+    @htable = HTable.new(hbase_hndl.hbconf, name)
+    @wildcard = wildcard
+    #@patterns = patterns
   end
   
   def add_pattern(pattern)
-    @java_impl.add_pattern((pattern.is_a? Array) ? pattern.to_java(:Long) : pattern)
-    # TODO: create a Scan object for the pattern.
-    @patterns << pattern
+    #@patterns << pattern
   end
   
   def [](key)
     getd = Get.new((key.is_a? Array) ? key.to_java(:byte) : key)
-    getd.addColumn(Bytes.toBytes("value:"))
+    getd.addColumn(Bytes.toBytes("value"))
     result = @htable.get(getd)
-    Bytes.toDouble(result.getValue())
+    Bytes.toDouble(result.value())
   end
   
   def []=(key, val)
     putd = Put.new((key.is_a? Array) ? key.to_java(:byte) : key)
-    key.each_index do |k_i| putd.add(Bytes.toBytes("key{#k_i.to_s}:"), Bytes.toBytes(key[k_i])) end
-    putd.add(Bytes.toBytes("value:"), Bytes.toBytes(val))
+    key.each_index do |k_i| putd.add(Bytes.toBytes("key#{k_i.to_s}"), nil, Bytes.toBytes(key[k_i])) end
+    putd.add(Bytes.toBytes("value"), nil, Bytes.toBytes(val))
     @htable.put(putd)
   end
   
   def has_key?(key)
     getd = Get.new((key.is_a? Array) ? key.to_java(:byte) : key)
-    getd.addColumn(Bytes.toBytes("value:"))
+    getd.addColumn(Bytes.toBytes("value"))
     @htable.exists(getd)
   end
   
@@ -79,7 +87,7 @@ class MultiKeyMap
     scanner = result_scanner.iterator();
     while (scanner.hasNext())
       r = scanner.next();
-      ret[r.getRow()] = r.getValue(Bytes.toBytes("value:"));
+      ret[r.getRow()] = r.getValue(Bytes.toBytes("value"));
     end
     result_scanner.close();
     ret
@@ -91,50 +99,52 @@ class MultiKeyMap
       return;
     end
     scand = Scan.new()
-    (0..@numkeys).each do |i| scand.addColumn("key#{i.to_s}:".to_java_bytes) end
-    scand.addColumn("value:".to_java_bytes)
+    (0...@numkeys).each do |i| scand.addColumn("key#{i.to_s}".to_java_bytes) end
+    scand.addColumn("value".to_java_bytes)
     
-    filterlist = FilterList.new()
+    filter_list = FilterList.new()
     partial_key.each_index do |k_i|
       unless partial_key[k_i] == @wildcard then
-        filterList.addFilter(SingleColumnValueFilter.new(
-          "key#{k_i.to_s}:", nil, CompareFilter.EQUAL, partial_key[k_i]));
+        filter_list.addFilter(SingleColumnValueFilter.new(
+          Bytes.toBytes("key#{k_i.to_s}"), nil,
+          CompareFilter::CompareOp::EQUAL, Bytes.toBytes(partial_key[k_i])));
       end 
     end
-    scand.setFilter(filterList)
+    scand.setFilter(filter_list)
     
     result_scanner = @htable.getScanner(scand);
     scanner = result_scanner.iterator();
     while (scanner.hasNext())
       r = scanner.next();
-      keys = (0..@numkeys).collect { |i| r.getValue(Bytes.toBytes("key#{i.to_s}:")) }
-      value = r.getValue(Bytes.toBytes("value:"));
-      yield keys, value.to_f;
+      keys = (0...@numkeys).collect { |i| Bytes.toLong(r.getValue(Bytes.toBytes("key#{i.to_s}"))) }
+      value = Bytes.toDouble(r.getValue(Bytes.toBytes("value")));
+      yield keys, value;
     end
     result_scanner.close();
   end
   
   def replace(partial_key)
     scand = Scan.new()
-    (0..@numkeys).each do |i| scand.addColumn("key#{i.to_s}:".to_java_bytes) end
-    scand.addColumn("value:".to_java_bytes)
+    (0...@numkeys).each do |i| scand.addColumn("key#{i.to_s}".to_java_bytes) end
+    scand.addColumn("value".to_java_bytes)
 
-    filterList = FilterList.new()
+    filter_list = FilterList.new()
     partial_key.each_index do |k_i|
       unless partial_key[k_i] == @wildcard then
-        filterList.addFilter(SingleColumnValueFilter.new(
-          "key#{k_i.to_s}:", nil, CompareFilter.EQUAL, partial_key[k_i]));
+        filter_list.addFilter(SingleColumnValueFilter.new(
+          Bytes.toBytes("key#{k_i.to_s}"), nil,
+          CompareFilter::CompareOp::EQUAL, Bytes.toBytes(partial_key[k_i])));
       end 
     end
-    scand.setFilterList(filterList)
+    scand.setFilterList(filter_list)
 
     result_scanner = @htable.getScanner(scand);
     scanner = result_scanner.iterator();
     while (scanner.hasNext())
       r = scanner.next();
-      keys = (0..@numkeys).collect { |i| r.getValue(Bytes.toBytes("key#{i.to_s}:")) }
-      value = r.getValue(Bytes.toBytes("value:"));
-      new_value = yield keys, value.to_f;
+      keys = (0...@numkeys).collect { |i| Bytes.toLong(r.getValue(Bytes.toBytes("key#{i.to_s}"))) }
+      value = Bytes.toDouble(r.getValue(Bytes.toBytes("value")));
+      new_value = yield keys, value;
       put(keys, new_value)
     end
     result_scanner.close();
@@ -151,20 +161,34 @@ class MultiKeyMap
   # Methods to return cursors (i.e. iterables) themselves.
   def cursor(partial_key)
     scand = Scan.new()
-    (0..@numkeys).each do |i| scand.addColumn("key#{i.to_s}:".to_java_bytes) end
-    scand.addColumn("value:".to_java_bytes)
+    (0...@numkeys).each do |i| scand.addColumn("key#{i.to_s}".to_java_bytes) end
+    scand.addColumn("value".to_java_bytes)
 
-    filterList = FilterList.new()
+    filter_list = FilterList.new()
     partial_key.each_index do |k_i|
       unless partial_key[k_i] == @wildcard then
-        filterList.addFilter(SingleColumnValueFilter.new(
-          "key#{k_i.to_s}:", nil, CompareFilter.EQUAL, partial_key[k_i]));
+        filter_list.addFilter(SingleColumnValueFilter.new(
+          Bytes.toBytes("key#{k_i.to_s}"), nil,
+          CompareFilter::CompareOp::EQUAL, Bytes.toBytes(partial_key[k_i])));
       end 
     end
-    scand.setFilterList(filterList)
+    scand.setFilterList(filter_list)
 
     result_scanner = @htable.getScanner(scand);
     result_scanner.iterator()
   end
 
 end
+
+def test
+  m = MultiKeyMap.new(2, [], "x")
+  keys = [[1,1], [1,2], [2,2]]
+  val = 1.0
+  keys.each do |k|  m[k] = val; val += 1.0; end
+  keys.each do |k|  x = m[k]; puts "Key #{k}: #{x}"; end
+  slices = [[1,-1], [-1,2]]
+  slices.each do |s| m.scan(s) { |k,v| puts "Key #{k}: #{v.to_s}" } end
+end
+
+# Unit test
+#test
