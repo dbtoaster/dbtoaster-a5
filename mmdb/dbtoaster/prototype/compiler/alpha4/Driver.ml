7c409c0c175a453e3bb3a7f576eb8a151c0af256
Oliver Kennedy <okennedy@buffalo.edu>
2010-04-07 21:01:15 +0000

    Commented Driver
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@872 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: c355b7ea28cbe4ecefcf2206b1962b8badfe4e8f

--- mmdb/dbtoaster/prototype/compiler/alpha4/Driver.ml
+++ mmdb/dbtoaster/prototype/compiler/alpha4/Driver.ml
@@ -1,3 +1,64 @@
+(*
+  Driver.ml
+  
+  The primary interface to the dbtoaster compiler; Compiles into dbtoaster
+  
+  Most of Driver is just glue code and parameter parsing options; The process
+  can be broken down into roughly 5 stages:
+  
+  1 - Parse user options.  Self explanatory.  For a full rundown, compile
+      and run './dbtoaster -?'  Among other things, this is where we learn
+      what language the user wants to compile into.  This is also where we
+      figure out where to output.  The global 'output_file' variable is set to
+      the appropriate output stream.
+      
+  2 - Translate SQL to Calculus.  Most of the heavy lifting here is done by
+      ocaml's builtin Parsing module, and the lexing/parsing modules in the
+      parser subdirectory (in particular Sqlparser).  
+      
+      After this phase we are left with a list of querysets (one per input
+      file).  Each queryset contains a list of queries, a schema, and a list of
+      relation input sources (M3.relation_input_t).  
+      
+      If the user indicates that they want their output in relational calculus
+      form (ie, "-l calc" is passed in on the command line), convert the 
+      generated formula to a string, dump it into the output file, and exit.
+  
+  3 - Translate Calculus to M3.  This is where most of the exciting stuff takes
+      place.  Most of the heavy lifting is done by the Compiler and CalcToM3 
+      modules: Compiler.compile and CalcToM3.M3InProgress.generate_m3.  
+      compile, when invoked with generate_m3 will output an M3InProgress.t that
+      we can repeatedly invoke Compiler.compile on.  
+      
+      Compiler.compile is invoked once per query (recall, queries are stored 
+      under 2 levels of list nesting), and an M3.prog_t is extracted from the
+      result.
+      
+  4 - Translate M3 to the target language.  If the target language is M3, then
+      we just need to turn it into a string before outputting it.  Otherwise,
+      we need to do some code generation.  The heavy lifting here is done by
+      the M3Compiler module parametrized by an M3Codegen.
+      
+      Each subdirectory of the codegen directory contains a subclass of 
+      M3Codegen.  Parametrized appropriately, M3Compiler translates an M3.prog_t
+      and a set of relation input sources (obtained in step 2) into the target
+      language.  Currently, support exists for compiling into:
+        - OCAML
+      
+      Unless "-l none" is specified, the compiler will compile the M3 program 
+      and output to the designated output_file (with one caveat, see step 5).
+      
+  5 - Invoke the secondary compiler.  If the target language is one with its own
+      (external) compiler (eg, Ocaml / C++), dbtoaster can be invoked with the
+      "-c" flag.  As a convenience, the "-c" flag will invoke the external 
+      compiler on the generated source file.  
+      
+      If no output file is specified for the target language (or if outputting 
+      to stdout), the M3->Target compiler will be invoked to produce output to 
+      a temporary file.  Consequently, unless explicitly directed to output to
+      stdout "-o -", only the temporary file will be produced (ie, the target
+      language output will not be dumped to stdout if "-c" is used).
+*)
 
 open Util
 
@@ -124,8 +185,7 @@ else ();;
 
 if language == L_SQL then
   (
-    (* failwith "Translation to normal SQL unsupported at the moment"; *)
-    exit 0
+    failwith "Translation to normal SQL unsupported at the moment";
   )
 else ();;
 
