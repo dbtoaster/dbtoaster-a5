037ac87619e9ef3aef4f0099ebf7cd3457c3df55
Yanif Ahmad <yanif.ahmad@gmail.com>
2010-12-05 22:54:36 +0000

    Cleaned up rst test queries.
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@1135 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: df237bb5a18f806418f02f0c1bcfddb4981c5ca9

--- mmdb/dbtoaster/prototype/compiler/alpha4/test/sql/rst2.sql
+++ mmdb/dbtoaster/prototype/compiler/alpha4/test/sql/rst2.sql
@@ -1,13 +1,26 @@
--- With the datafiles in test/data, this should produce
--- QUERY_1_1 = 18753367048934
--- or as the Ocaml outputter puts it:
--- QUERY_1_1: [[  ]->[[  ]->1.87533670489e+13;]<pat=>;]<pat=>
+-- SQL query that results in a statement with 2 input maps
+-- with different variables, i.e.:
+--
+-- ( QUERY_1_1[  ][  ] := 0. ) +=
+--  ( ( QUERY_1_1S1[ QUERY_1_1R1T1_initS_S__B ][  ] := 
+--      ( IF ( ( R__B < QUERY_1_1R1T1_initS_S__B ) )
+--        THEN ( ( QUERY_1_1S1_init[  ][ R__B ] := 0. ) )) )
+--     * 
+--    ( QUERY_1_1R1S1[ QUERY_1_1R1T1_initS_S__C ][  ] := 
+--      ( IF ( ( QUERY_1_1R1T1_initS_S__C < T__C ) )
+--        THEN ( ( QUERY_1_1R1S1_init[  ][ T__C ] := 0. ) )) )
+--   )
 
 CREATE TABLE R(A int, B int) 
-  FROM POSTGRES dbtoaster.R(A int, B int);
+  FROM FILE 'test/data/r.dat' LINE DELIMITED
+  csv (fields := ',', schema := 'int,int', eventtype := 'insert');
+
 CREATE TABLE S(B int, C int) 
-  FROM POSTGRES dbtoaster.S(B int, C int);
+  FROM FILE 'test/data/s.dat' LINE DELIMITED
+  csv (fields := ',', schema := 'int,int', eventtype := 'insert');
+
 CREATE TABLE T(C int, D int)
-  FROM POSTGRES dbtoaster.T(C int, D int);
+  FROM FILE 'test/data/t.dat' LINE DELIMITED
+  csv (fields := ',', schema := 'int,int', eventtype := 'insert');
 
 SELECT sum(A*D) FROM R,S,T WHERE R.B<S.B AND S.C<T.C;
