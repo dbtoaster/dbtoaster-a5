726cd985ae07f47c550d746fc76f72636339e4aa
Yanif Ahmad <yanif.ahmad@gmail.com>
2010-12-12 01:03:01 +0000

    Updated finance sql queries.
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@1156 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: d30036bf9d16f64339a75aeb5e645dce12b47d5e

--- mmdb/dbtoaster/prototype/compiler/alpha4/test/sql/finance/missedtrades.sql
+++ mmdb/dbtoaster/prototype/compiler/alpha4/test/sql/finance/missedtrades.sql
@@ -1,21 +1,15 @@
-CREATE TABLE BIDS (t double, id int, broker_id int, p double, v double);
-CREATE TABLE ASKS (t double, id int, broker_id int, p double, v double);
+CREATE TABLE bids(broker_id float, v float, p float)
+  FROM FILE 'test/data/vwap5k.csv'
+  LINE DELIMITED orderbook (book := 'bids', validate := 'true', brokers := '10');
 
---select b.broker_id, sum(a.p*a.v+-1*(b.p*b.v))
---from bids b, asks a
---where 0.25*(select sum(a1.v) from asks a1) >
---           (select sum(a2.v) from asks a2 where a2.p > a.p)
---and   0.25*(select sum(b1.v) from bids b1) >
---           (select sum(b2.v) from bids b2 where b2.p > b.p)
---group by b.broker_id
+CREATE TABLE asks(broker_id float, v float, p float)
+  FROM FILE 'test/data/vwap5k.csv'
+  LINE DELIMITED orderbook (book := 'asks', validate := 'true', brokers := '10');
 
--- simplified form for debugging
-select b.broker_id, sum((a.p*a.v) + ((-1) * (b.p*b.v)))
+select b.broker_id, sum(a.p*a.v+((-1)*(b.p*b.v)))
 from bids b, asks a
-where 
---      0.25*(select sum(a1.v) from asks a1) >
---           (select sum(a2.v) from asks a2 where a2.p > a.p)
---and
-      0.25*(select sum(b1.v) from bids b1) >
+where 0.25*(select sum(a1.v) from asks a1) >
+           (select sum(a2.v) from asks a2 where a2.p > a.p)
+and   0.25*(select sum(b1.v) from bids b1) >
            (select sum(b2.v) from bids b2 where b2.p > b.p)
 group by b.broker_id
--- mmdb/dbtoaster/prototype/compiler/alpha4/test/sql/finance/pricespread.sql
+++ mmdb/dbtoaster/prototype/compiler/alpha4/test/sql/finance/pricespread.sql
@@ -1,5 +1,3 @@
---CREATE TABLE BIDS (t double, id int, broker_id int, p double, v double);
---CREATE TABLE ASKS (t double, id int, broker_id int, p double, v double);
 CREATE TABLE bids(broker_id float, v float, p float)
   FROM FILE 'test/data/vwap5k.csv'
   LINE DELIMITED orderbook (book := 'bids', validate := 'true', brokers := '10');
@@ -10,10 +8,6 @@ CREATE TABLE asks(broker_id float, v float, p float)
 
 
 -- look at spread between significant orders
---select sum(a.p+-1*b.p) from bids b, asks a
---where ( b.v>0.0001*(select sum(b1.v) from bids b1) )
---and ( a.v>0.0001*(select sum(a1.v) from asks a1) )
-
--- simplified form for debugging
-select sum(a.p+b.p) from bids b, asks a
-where ( b.v > 0.0001*(select sum(b1.v) from bids b1) )
+select sum(a.p+((-1)*b.p)) from bids b, asks a
+where ( b.v>0.0001*(select sum(b1.v) from bids b1) )
+and ( a.v>0.0001*(select sum(a1.v) from asks a1) )
