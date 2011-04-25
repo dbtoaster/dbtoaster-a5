4c7141dc7a472b85e668bddbd863ee8f34fb8d81
Oliver Kennedy <okennedy@buffalo.edu>
2011-04-25 03:16:27 +0000

    updating unit tests
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@1265 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 1c09fe86364bf81b4d6984fef05eff1cf3d64ab2

--- dbtoaster/compiler/alpha5/test/unit/calc_translation.ml
+++ dbtoaster/compiler/alpha5/test/unit/calc_translation.ml
@@ -26,17 +26,17 @@ let sc = ("S_C",IntegerT) in
 let tc = ("T_C",IntegerT) in
 let td = ("T_D",IntegerT) in
    translate_test "test/sql/rst.sql" [
-      Leaf(AggSum([],
+      AggSum([],
          (Prod[
-            Leaf(Relation("R", [ra;rb]));
-            Leaf(Relation("S", [sb;sc]));
-            Leaf(Relation("T", [tc;td]));
-            Leaf(Cmp(Var(rb),Eq,Var(sb)));
-            Leaf(Cmp(Var(sc),Eq,Var(tc)));
-            Leaf(Value(Var(ra)));
-            Leaf(Value(Var(td)))
+            Relation("R", [ra;rb]);
+            Relation("S", [sb;sc]);
+            Relation("T", [tc;td]);
+            Cmp(Var(rb),Eq,Var(sb));
+            Cmp(Var(sc),Eq,Var(tc));
+            Value(Var(ra));
+            Value(Var(td))
          ])
-      ))
+      )
    ]
 ;;
 let ra = ("R_A",IntegerT) in
@@ -44,40 +44,40 @@ let rb = ("R_B",IntegerT) in
 let sc = ("S_C",IntegerT) in
 let sd = ("S_D",IntegerT) in
    translate_test "test/sql/rs_inequality.sql" [
-      Leaf(AggSum([],
+      AggSum([],
          (Prod[
-            Leaf(Relation("R", [ra;rb]));
-            Leaf(Relation("S", [sc;sd]));
-            Leaf(Cmp(Var(rb),Lt,Var(sc)));
-            Leaf(Value(Var(ra)));
-            Leaf(Value(Var(sc)))
+            Relation("R", [ra;rb]);
+            Relation("S", [sc;sd]);
+            Cmp(Var(rb),Lt,Var(sc));
+            Value(Var(ra));
+            Value(Var(sc))
          ])
-      ))
+      )
    ]
 ;;
 let p i = (("B"^(string_of_int i)^"_PRICE"), DoubleT) in
 let v i = (("B"^(string_of_int i)^"_VOLUME"), IntegerT) in
    translate_test "test/sql/finance/vwap.sql" [
-      Leaf(AggSum([],
+      AggSum([],
          (Prod[
-            Leaf(Relation("BIDS", [p 1; v 1]));
-            Leaf(Definition(("TMP_0", DoubleT), (Prod[
-               Leaf(Value(Const(Double(0.25))));
-               Leaf(AggSum([],
+            Relation("BIDS", [p 1; v 1]);
+            Definition(("TMP_0", DoubleT), (Prod[
+               Value(Const(Double(0.25)));
+               AggSum([],
                   (Prod[
-                     Leaf(Relation("BIDS", [p 3; v 3]));
-                     Leaf(Value(Var(v 3)))
+                     Relation("BIDS", [p 3; v 3]);
+                     Value(Var(v 3))
                   ])
-               ))
-            ])));
-            Leaf(Definition(("TMP_1", IntegerT), (Leaf(AggSum([], (Prod[
-               Leaf(Relation("BIDS", [p 2; v 2]));
-               Leaf(Cmp((Var(p 2)), Gt, (Var(p 1))));
-               Leaf(Value(Var(v 2)))
-            ]))))));
-            Leaf(Cmp((Var("TMP_0", DoubleT)), Gt, (Var("TMP_1", IntegerT))));
-            Leaf(Value(Var(p 1)));
-            Leaf(Value(Var(v 1)))
+               )
+            ]));
+            Definition(("TMP_1", IntegerT), (AggSum([], (Prod[
+               Relation("BIDS", [p 2; v 2]);
+               Cmp((Var(p 2)), Gt, (Var(p 1)));
+               Value(Var(v 2))
+            ]))));
+            Cmp((Var("TMP_0", DoubleT)), Gt, (Var("TMP_1", IntegerT)));
+            Value(Var(p 1));
+            Value(Var(v 1))
          ])
-      ))
+      )
    ]
