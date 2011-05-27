0130b5bd1f75974cd61ea27c11262b843bc50831
Kunal Shah <kunalshah@dbtoaster.org>
2011-05-27 08:07:02 +0000

    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@1283 e0e46c49-be69-4f5a-ad62-21024a331aea
    Former-commit-id: 18af4d24234a5447debd4a2a2b19dc055644e451

--- dbtoaster/applications/trading/alpha2/StockExchangeSimOpt/src/algotraders/HistoricTrader.java
+++ dbtoaster/applications/trading/alpha2/StockExchangeSimOpt/src/algotraders/HistoricTrader.java
@@ -5,14 +5,12 @@
 package algotraders;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
+import java.util.Date;
 import java.util.concurrent.Executors;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.jboss.netty.bootstrap.ClientBootstrap;
 import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.ChannelFactory;
@@ -21,8 +19,6 @@ import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.ChannelPipelineFactory;
 import org.jboss.netty.channel.Channels;
 import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
-import org.jboss.netty.handler.codec.frame.DelimiterBasedFrameDecoder;
-import org.jboss.netty.handler.codec.frame.Delimiters;
 import org.jboss.netty.handler.codec.string.StringDecoder;
 import org.jboss.netty.handler.codec.string.StringEncoder;
 
@@ -39,7 +35,7 @@ public class HistoricTrader {
         System.out.println("Enter test file name");
         BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
         String filename = b.readLine();
-        
+
         reader = new BufferedReader(new FileReader(filename));
 
         //Establish connection
@@ -69,19 +65,35 @@ public class HistoricTrader {
             return;
         }
         int order_id = 0;
-        while ((order = reader.readLine()) != null) {
-            String fields[] = order.split(",");
+
+        //Process the first entry to synchronise the historic data timestamp.
+        order = reader.readLine();
+        String fields[] = order.split(",");
+        long startHistoricTimestamp = Long.parseLong(fields[0]);
+        long startSimTimestamp = new Date().getTime();
+        
+        do {
+            fields = order.split(",");
             int stock_id = 10101;
             double price = (Integer.valueOf(fields[4])) * 1.0 / 10000;
             int volume = (Integer.valueOf(fields[3]));
             String action = fields[2];
             int traderId = -1;
-            if(action.equals("D"))continue;
+            long timestamp = Long.parseLong(fields[0]);
+            if (action.equals("D")) {
+                continue;
+            }
             String message = String.format("%s;stock_id:%s price:%s volume:%s order_id:%s trader:%s\n", action, stock_id, price, volume, order_id++, traderId);
+            /*while((new Date().getTime()-startSimTimestamp) < (timestamp - startHistoricTimestamp)){
+                System.out.println("This is order no. "+order_id+" waiting for "+ (new Date().getTime()-startSimTimestamp) +" to become "+ (timestamp - startHistoricTimestamp));
+            }*/
             
             cf = ch.write(message);
-            
+
             cf.awaitUninterruptibly();
-        }
+        } while ((order = reader.readLine()) != null && order_id<5000);
+        long endSimTimestamp = new Date().getTime();
+        System.out.println("Sent at rate = "+ 5000*1000 / (endSimTimestamp-startSimTimestamp));
+        System.out.println("Loop ended");
     }
 }
