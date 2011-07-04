216791c2ce8744e7b9b5d9c3a12e8a11f572b16e
Oliver Kennedy <okennedy@buffalo.edu>
2011-07-04 11:25:27 +0000

    Adding 'deterministic' to test/sql/finance/missedtrades.sql Adding bin directory to makefile for compiled queries.
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@1320 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 8c3ca65271de847c8377e32db1a63635981bfe9c

--- dbtoaster/compiler/alpha4/test/sql/finance/missedtrades.sql
+++ dbtoaster/compiler/alpha4/test/sql/finance/missedtrades.sql
@@ -16,11 +16,13 @@
 
 CREATE TABLE bids(t float, id int, broker_id int, volume float, price float)
   FROM FILE 'test/data/vwap5k.csv'
-  LINE DELIMITED orderbook (book := 'bids', brokers := '10');
+  LINE DELIMITED orderbook (book := 'bids', brokers := '10',
+                            deterministic := 'yes');
 
 CREATE TABLE asks(t float, id int, broker_id int, volume float, price float)
   FROM FILE 'test/data/vwap5k.csv'
-  LINE DELIMITED orderbook (book := 'asks', brokers := '10');
+  LINE DELIMITED orderbook (book := 'asks', brokers := '10',
+                            deterministic := 'yes');
 
 select b.broker_id, sum(a.price*a.volume+-1*b.price*b.volume)
 from bids b, asks a
