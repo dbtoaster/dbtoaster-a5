9f60f0ad4a54710569c50d64260f2f5358ef3452
Oliver Kennedy <okennedy@buffalo.edu>
2011-09-02 14:57:09 +0000

    adding a simple self-join star query to the test process -- the query compilation fails for some reason, I'm tracking it down.
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@1404 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 5c52999d80227083d175a3ed96ee260b598b8ddf

--- dbtoaster/compiler/alpha4/test/scripts/query_test.rb
+++ dbtoaster/compiler/alpha4/test/scripts/query_test.rb
@@ -31,6 +31,11 @@ $queries = {
     :answer => 18753367048934.0,
     :valid_opts => ["depth-1"]
   },
+  "rstar" => {
+    :path => "test/sql/simple/rstar.sql",
+    :type => :singleton,
+    :answer => 0
+  },
   "vwap" => {
     :path => "test/sql/finance/vwap.sql",
     :type => :singleton,
