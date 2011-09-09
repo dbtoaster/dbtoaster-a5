b612a317a825ab59ce2c1ab8e0e7edad67f98ae6
Oliver Kennedy <okennedy@buffalo.edu>
2011-09-09 09:27:31 +0000

    This doesn't work.
    Why doesn't this work?
    
    *glare*
    
    
    
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@1421 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: fa9dc4296d440ea8d6d02f8ebc2b6e42f4d400fa

---
+++ dbtoaster/compiler/alpha4/test/sql/simple/rstar_supersimple.sql
@@ -0,0 +1,26 @@
+
+CREATE TABLE R(A int, B int) 
+  FROM FILE 'test/data/r.dat' LINE DELIMITED
+  csv (fields := ',', schema := 'int,int', eventtype := 'insert');
+
+CREATE TABLE S(B int, C int) 
+  FROM FILE 'test/data/s.dat' LINE DELIMITED
+  csv (fields := ',', schema := 'int,int', eventtype := 'insert');
+
+SELECT sum(1) FROM R rb, R rc WHERE rb.A = rc.A;
+
+--SELECT sum(1) 
+--FROM --R ra, 
+--     R rb, 
+--     R rc--, 
+--     --S sa, 
+--     --S sb, 
+--     --S sc
+--WHERE --ra.A = rb.A AND 
+--      rb.A = rc.A-- AND
+--      --ra.B = sa.B AND 
+--      --rb.B = sb.B AND 
+--      --rc.B = sc.B
+----GROUP BY --sb.B, 
+--         --sc.C
+--;
