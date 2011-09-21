020542925b95216baa51c3f379e31326def04e31
Oliver Kennedy <okennedy@buffalo.edu>
2011-09-21 09:31:39 +0000

    Debugging for (re: #45) * Isolated a case where the apply chaining K3 optimization fails * Added K3Optimizer.inline_collection_functions to the K3Optimizer interface (with an annotation that it's for debugging only) * Added ssb4_debug to the unit test list.
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@1441 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 10137732a6d202c3d1aec0dab922702cc5531e88

--- dbtoaster/compiler/alpha4/stages/functional/K3Optimizer.mli
+++ dbtoaster/compiler/alpha4/stages/functional/K3Optimizer.mli
@@ -2,3 +2,9 @@ type optimization_t = CSE | Beta
 val optimize :
   ?optimizations: optimization_t list
   -> M3.var_t list -> K3.SR.expr_t -> K3.SR.expr_t
+
+(******** Debugging interface.  DO NOT USE ******)
+val inline_collection_functions :
+   ((K3.SR.id_t * K3.SR.type_t) * (K3.SR.expr_t)) list ->
+   K3.SR.expr_t ->
+   K3.SR.expr_t
--- dbtoaster/compiler/alpha4/test/unit/ssb4_debug.ml
+++ dbtoaster/compiler/alpha4/test/unit/ssb4_debug.ml
@@ -4,143 +4,98 @@ open K3.SR
 open Util
 ;;
 
-Debug.activate "K3-TYPECHECK-DETAIL"
+(* Debug.activate "K3-TYPECHECK-DETAIL";; *)
 
-;;
-
-let pc_lookup_type = (K3.SR.Singleton((K3.SR.Lookup((K3.SR.PC("QUERY_1_1_pNATION1_pORDERS1_pLINEITEM1_pCUSTOMER1",["QUERY_1_1_pCUSTOMER1_pORDERS1_pLINEITEM2_pSUPPLIER1NATION_CN__NATIONKEY",TFloat],["QUERY_1_1NATION_CN__NATIONKEY",TFloat;"QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat;"TYPE",TFloat;"QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat],TFloat)),[ (K3.SR.Var("QUERY_1_1_pCUSTOMER1_pORDERS1_pLINEITEM2_pSUPPLIER1NATION_CN__NATIONKEY",(K3.SR.TFloat))) ]))))
+let pc_lookup_type = (K3.SR.Lookup((K3.SR.PC("QUERY_1_1_pNATION1_pORDERS1_pLINEITEM1_pCUSTOMER1",["QUERY_1_1_pCUSTOMER1_pORDERS1_pLINEITEM2_pSUPPLIER1NATION_CN__NATIONKEY",TFloat],["QUERY_1_1NATION_CN__NATIONKEY",TFloat;"QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat;"TYPE",TFloat;"QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat],TFloat)),[ (K3.SR.Var("QUERY_1_1_pCUSTOMER1_pORDERS1_pLINEITEM2_pSUPPLIER1NATION_CN__NATIONKEY",(K3.SR.TFloat))) ]))
 ;;
 Debug.log_unit_test "PC Lookup Type"
    K3.SR.string_of_type
    (K3Typechecker.typecheck_expr pc_lookup_type)
-   (Collection(TTuple[
-      TFloat; 
-      Collection(TTuple[TFloat;TFloat;TFloat;TFloat;TFloat])
+   (Collection(TTuple[TFloat;TFloat;TFloat;TFloat;TFloat]))
+;;
+
+let buggy_apply_nesting = 
+   (Apply(Lambda(AVar("current_slice",Collection(TTuple[TFloat ; TFloat ; TFloat ; TFloat])),
+          Var("current_slice",Collection(TTuple[TFloat ; TFloat ; TFloat ; TFloat]))),
+        Var("existing_slice",Collection(TTuple[TFloat ; TFloat ; TFloat ; TFloat]))))
+;;
+Debug.log_unit_test "Inner Apply Chaining"
+   K3.SR.string_of_expr
+   (K3Optimizer.inline_collection_functions
+      [("existing_slice",Collection(TTuple[TFloat ; TFloat ; TFloat ; TFloat])),
+         Lookup(
+            PC("QUERY_1_1_pNATION1_pORDERS1_pCUSTOMER1",["NATIONKEY",TFloat],["QUERY_1_1NATION_CN__NATIONKEY",TFloat;"QUERY_1_1_pNATION1ORDERS_ORDERS__ORDERKEY",TFloat;"TYPE",TFloat],TFloat),
+            [Var("NATIONKEY",TFloat);
+         ])
+      ]
+      buggy_apply_nesting
+   )
+   (Lookup(
+      PC("QUERY_1_1_pNATION1_pORDERS1_pCUSTOMER1",["NATIONKEY",TFloat],["QUERY_1_1NATION_CN__NATIONKEY",TFloat;"QUERY_1_1_pNATION1ORDERS_ORDERS__ORDERKEY",TFloat;"TYPE",TFloat],TFloat),
+      [Var("NATIONKEY",TFloat);
    ]))
-;;
-
-let buggy_stack = 
-(Iterate(
-     Lambda(ATuple(["QUERY_1_1_pCUSTOMER1_pORDERS1_pLINEITEM2_pSUPPLIER1NATION_CN__NATIONKEY",TFloat;"existing_slice",Collection(TTuple[TFloat ; TFloat ; TFloat ; TFloat ; TFloat])]),
-       Iterate(
-         Lambda(ATuple(["QUERY_1_1NATION_CN__NATIONKEY",TFloat;"QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat;"TYPE",TFloat;"QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat;"dv",TFloat]),
-           IfThenElse(
-             Member(
-               Var("existing_slice",Collection(TTuple[TFloat ; TFloat ; TFloat ; TFloat ; TFloat])),
-               [Var("QUERY_1_1NATION_CN__NATIONKEY",TFloat);
-               Var("QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat);
-               Var("TYPE",TFloat);
-               Var("QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat);
-               ]),
-             PCValueUpdate(
-               PC("QUERY_1_1_pNATION1_pORDERS1_pLINEITEM1_pCUSTOMER1",["QUERY_1_1_pCUSTOMER1_pORDERS1_pLINEITEM2_pSUPPLIER1NATION_CN__NATIONKEY",TFloat],["QUERY_1_1NATION_CN__NATIONKEY",TFloat;"QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat;"TYPE",TFloat;"QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat],TFloat),
-               [Var("QUERY_1_1_pCUSTOMER1_pORDERS1_pLINEITEM2_pSUPPLIER1NATION_CN__NATIONKEY",TFloat);
-               ],[Var("QUERY_1_1NATION_CN__NATIONKEY",TFloat);
-               Var("QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat);
-               Var("TYPE",TFloat);
-               Var("QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat);
-               ],
-               Add(
-                 Lookup(
-                   Var("existing_slice",Collection(TTuple[TFloat ; TFloat ; TFloat ; TFloat ; TFloat])),
-                   [Var("QUERY_1_1NATION_CN__NATIONKEY",TFloat);
-                   Var("QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat);
-                   Var("TYPE",TFloat);
-                   Var("QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat);
-                   ]),Var("dv",TFloat))),
-             PCValueUpdate(
-               PC("QUERY_1_1_pNATION1_pORDERS1_pLINEITEM1_pCUSTOMER1",["QUERY_1_1_pCUSTOMER1_pORDERS1_pLINEITEM2_pSUPPLIER1NATION_CN__NATIONKEY",TFloat],["QUERY_1_1NATION_CN__NATIONKEY",TFloat;"QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat;"TYPE",TFloat;"QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat],TFloat),
-               [Var("QUERY_1_1_pCUSTOMER1_pORDERS1_pLINEITEM2_pSUPPLIER1NATION_CN__NATIONKEY",TFloat);
-               ],[Var("QUERY_1_1NATION_CN__NATIONKEY",TFloat);
-               Var("QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat);
-               Var("TYPE",TFloat);
-               Var("QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat);
-               ],
-               Add(
-                 Lookup(
-                   GroupByAggregate(
-                     AssocLambda(ATuple(["QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat;"SUPPLIER__NAME",TFloat;"SUPPLIER__ADDRESS",TFloat;"QUERY_1_1NATION_CN__NATIONKEY",TFloat;"SUPPLIER__PHONE",TFloat;"SUPPLIER__ACCTBAL",TFloat;"SUPPLIER__COMMENT",TFloat;"QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat;"PART__NAME",TFloat;"MFGR",TFloat;"BRAND",TFloat;"TYPE",TFloat;"SIZE",TFloat;"CONTAINER",TFloat;"RETAILPRICE",TFloat;"PART__COMMENT",TFloat;"QUERY_1_1_pCUSTOMER1_pORDERS1_pLINEITEM2_pSUPPLIER1NATION_CN__NATIONKEY",TFloat;"CN__NAME",TFloat;"CN__REGIONKEY",TFloat;"CN__COMMENT",TFloat;"v",TFloat]),AVar("accv",TFloat),
-                       Add(Var("v",TFloat),Var("accv",TFloat))),Const(CFloat(0.)),
-                     Lambda(ATuple(["QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat;"SUPPLIER__NAME",TFloat;"SUPPLIER__ADDRESS",TFloat;"QUERY_1_1NATION_CN__NATIONKEY",TFloat;"SUPPLIER__PHONE",TFloat;"SUPPLIER__ACCTBAL",TFloat;"SUPPLIER__COMMENT",TFloat;"QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat;"PART__NAME",TFloat;"MFGR",TFloat;"BRAND",TFloat;"TYPE",TFloat;"SIZE",TFloat;"CONTAINER",TFloat;"RETAILPRICE",TFloat;"PART__COMMENT",TFloat;"QUERY_1_1_pCUSTOMER1_pORDERS1_pLINEITEM2_pSUPPLIER1NATION_CN__NATIONKEY",TFloat;"CN__NAME",TFloat;"CN__REGIONKEY",TFloat;"CN__COMMENT",TFloat;"v",TFloat]),
-                       Tuple[Var("QUERY_1_1NATION_CN__NATIONKEY",TFloat);
-                         Var("QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat);
-                         Var("TYPE",TFloat);
-                         Var("QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat)]),
-                     Flatten(
-                       Map(
-                         Lambda(ATuple(["QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat;"SUPPLIER__NAME",TFloat;"SUPPLIER__ADDRESS",TFloat;"QUERY_1_1NATION_CN__NATIONKEY",TFloat;"SUPPLIER__PHONE",TFloat;"SUPPLIER__ACCTBAL",TFloat;"SUPPLIER__COMMENT",TFloat;"v1",TFloat]),
-                           Flatten(
-                             Map(
-                               Lambda(ATuple(["QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat;"PART__NAME",TFloat;"MFGR",TFloat;"BRAND",TFloat;"TYPE",TFloat;"SIZE",TFloat;"CONTAINER",TFloat;"RETAILPRICE",TFloat;"PART__COMMENT",TFloat;"var45",TFloat]),
-                                 Map(
-                                   Lambda(ATuple(["QUERY_1_1_pCUSTOMER1_pORDERS1_pLINEITEM2_pSUPPLIER1NATION_CN__NATIONKEY",TFloat;"CN__NAME",TFloat;"CN__REGIONKEY",TFloat;"CN__COMMENT",TFloat;"var43",TFloat]),
-                                     Tuple[
-                                       Var("QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat);
-                                       Var("SUPPLIER__NAME",TFloat);
-                                       Var("SUPPLIER__ADDRESS",TFloat);
-                                       Var("QUERY_1_1NATION_CN__NATIONKEY",TFloat);
-                                       Var("SUPPLIER__PHONE",TFloat);
-                                       Var("SUPPLIER__ACCTBAL",TFloat);
-                                       Var("SUPPLIER__COMMENT",TFloat);
-                                       Var("QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat);
-                                       Var("PART__NAME",TFloat);
-                                       Var("MFGR",TFloat);Var("BRAND",TFloat);
-                                       Var("TYPE",TFloat);Var("SIZE",TFloat);
-                                       Var("CONTAINER",TFloat);
-                                       Var("RETAILPRICE",TFloat);
-                                       Var("PART__COMMENT",TFloat);
-                                       Var("QUERY_1_1_pCUSTOMER1_pORDERS1_pLINEITEM2_pSUPPLIER1NATION_CN__NATIONKEY",TFloat);
-                                       Var("CN__NAME",TFloat);
-                                       Var("CN__REGIONKEY",TFloat);
-                                       Var("CN__COMMENT",TFloat);
-                                       IfThenElse0(Const(CFloat(1.)),
-                                         Mult(Var("v1",TFloat),
-                                           Mult(Var("var45",TFloat),
-                                             Add(Var("var43",TFloat),
-                                               IfThenElse0(
-                                                 Eq(
-                                                   Var("QUERY_1_1NATION_CN__NATIONKEY",TFloat),
-                                                   Var("QUERY_1_1_pCUSTOMER1_pORDERS1_pLINEITEM2_pSUPPLIER1NATION_CN__NATIONKEY",TFloat)),
-                                                 Const(CFloat(1.)))))))]),
-                                   Slice(OutPC("NATION",["QUERY_1_1_pCUSTOMER1_pORDERS1_pLINEITEM2_pSUPPLIER1NATION_CN__NATIONKEY",TFloat;"CN__NAME",TFloat;"CN__REGIONKEY",TFloat;"CN__COMMENT",TFloat],TFloat),["QUERY_1_1_pCUSTOMER1_pORDERS1_pLINEITEM2_pSUPPLIER1NATION_CN__NATIONKEY",TFloat;"CN__NAME",TFloat;"CN__REGIONKEY",TFloat;"CN__COMMENT",TFloat],["QUERY_1_1_pCUSTOMER1_pORDERS1_pLINEITEM2_pSUPPLIER1NATION_CN__NATIONKEY",(
-                                     Var("QUERY_1_1_pCUSTOMER1_pORDERS1_pLINEITEM2_pSUPPLIER1NATION_CN__NATIONKEY",TFloat));]))),
-                               Slice(OutPC("PART",["QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat;"PART__NAME",TFloat;"MFGR",TFloat;"BRAND",TFloat;"TYPE",TFloat;"SIZE",TFloat;"CONTAINER",TFloat;"RETAILPRICE",TFloat;"PART__COMMENT",TFloat],TFloat),["QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat;"PART__NAME",TFloat;"MFGR",TFloat;"BRAND",TFloat;"TYPE",TFloat;"SIZE",TFloat;"CONTAINER",TFloat;"RETAILPRICE",TFloat;"PART__COMMENT",TFloat],["QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",(
-                                 Var("QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat));"TYPE",(
-                                 Var("TYPE",TFloat));])))),
-                         Slice(OutPC("SUPPLIER",["QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat;"SUPPLIER__NAME",TFloat;"SUPPLIER__ADDRESS",TFloat;"QUERY_1_1NATION_CN__NATIONKEY",TFloat;"SUPPLIER__PHONE",TFloat;"SUPPLIER__ACCTBAL",TFloat;"SUPPLIER__COMMENT",TFloat],TFloat),["QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat;"SUPPLIER__NAME",TFloat;"SUPPLIER__ADDRESS",TFloat;"QUERY_1_1NATION_CN__NATIONKEY",TFloat;"SUPPLIER__PHONE",TFloat;"SUPPLIER__ACCTBAL",TFloat;"SUPPLIER__COMMENT",TFloat],["QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",(
-                           Var("QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat));"QUERY_1_1NATION_CN__NATIONKEY",(
-                           Var("QUERY_1_1NATION_CN__NATIONKEY",TFloat));])))),
-                   [Var("QUERY_1_1NATION_CN__NATIONKEY",TFloat);
-                   Var("QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat);
-                   Var("TYPE",TFloat);
-                   Var("QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat);
-                   ]),Var("dv",TFloat))))),
-         GroupByAggregate(
-           AssocLambda(ATuple(["QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat;"TYPE",TFloat;"QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat;"QUERY_1_1NATION_CN__NATIONKEY",TFloat;"v",TFloat]),AVar("accv",TFloat),
-             Add(Var("v",TFloat),Var("accv",TFloat))),Const(CFloat(0.)),
-           Lambda(ATuple(["QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat;"TYPE",TFloat;"QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat;"QUERY_1_1NATION_CN__NATIONKEY",TFloat;"v",TFloat]),
-             Tuple[Var("QUERY_1_1NATION_CN__NATIONKEY",TFloat);
-               Var("QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat);
-               Var("TYPE",TFloat);
-               Var("QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat)]),
-           Flatten(
-             Map(
-               Lambda(ATuple(["QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat;"TYPE",TFloat;"v1",TFloat]),
-                 Map(
-                   Lambda(ATuple(["QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat;"QUERY_1_1NATION_CN__NATIONKEY",TFloat;"v2",TFloat]),
-                     Tuple[
-                       Var("QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat);
-                       Var("TYPE",TFloat);
-                       Var("QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat);
-                       Var("QUERY_1_1NATION_CN__NATIONKEY",TFloat);
-                       Mult(Var("v1",TFloat),Var("v2",TFloat))]),
-                   Slice(OutPC("QUERY_1_1_pCUSTOMER1_pORDERS1_pLINEITEM2_pNATION1",["QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat;"QUERY_1_1NATION_CN__NATIONKEY",TFloat],TFloat),["QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat;"QUERY_1_1NATION_CN__NATIONKEY",TFloat],[]))),
-               Slice(OutPC("QUERY_1_1_pCUSTOMER1_pORDERS1_pLINEITEM1",["QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat;"TYPE",TFloat],TFloat),["QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat;"TYPE",TFloat],[])))))),
-     Singleton(
-       Lookup(
-         PC("QUERY_1_1_pNATION1_pORDERS1_pLINEITEM1_pCUSTOMER1",["QUERY_1_1_pCUSTOMER1_pORDERS1_pLINEITEM2_pSUPPLIER1NATION_CN__NATIONKEY",TFloat],["QUERY_1_1NATION_CN__NATIONKEY",TFloat;"QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__PARTKEY",TFloat;"TYPE",TFloat;"QUERY_1_1_pNATION1_pORDERS1LINEITEM_LINEITEM__SUPPKEY",TFloat],TFloat),
-         [Var("QUERY_1_1_pCUSTOMER1_pORDERS1_pLINEITEM2_pSUPPLIER1NATION_CN__NATIONKEY",TFloat);
-         ]))))
 
 ;;
-K3Typechecker.typecheck_expr buggy_stack
+
+let buggy_apply_opt = 
+   Apply(
+      Lambda(
+         AVar("existing_slice",
+            Collection(TTuple[TFloat ; TFloat ; TFloat ; TFloat])),
+               Iterate(
+                  Lambda(ATuple([
+                     "QUERY_1_1NATION_CN__NATIONKEY",TFloat;
+                     "QUERY_1_1_pNATION1ORDERS_ORDERS__ORDERKEY",TFloat;
+                     "TYPE",TFloat;
+                     "updated_v",TFloat]
+                  ),
+                  PCValueUpdate(
+                     PC("QUERY_1_1_pNATION1_pORDERS1_pCUSTOMER1",
+                        ["NATIONKEY",TFloat],
+                        
+                        ["QUERY_1_1NATION_CN__NATIONKEY",TFloat;
+                        "QUERY_1_1_pNATION1ORDERS_ORDERS__ORDERKEY",TFloat;
+                        "TYPE",TFloat],TFloat
+                     ),
+[Var("NATIONKEY",TFloat);
+          ],[Var("QUERY_1_1NATION_CN__NATIONKEY",TFloat);
+          Var("QUERY_1_1_pNATION1ORDERS_ORDERS__ORDERKEY",TFloat);
+          Var("TYPE",TFloat);],Var("updated_v",TFloat))),
+      buggy_apply_nesting)),
+  Lookup(
+    PC("QUERY_1_1_pNATION1_pORDERS1_pCUSTOMER1",["NATIONKEY",TFloat],["QUERY_1_1NATION_CN__NATIONKEY",TFloat;"QUERY_1_1_pNATION1ORDERS_ORDERS__ORDERKEY",TFloat;"TYPE",TFloat],TFloat),
+    [Var("NATIONKEY",TFloat);
+    ]))
+;;
+
+Debug.log_unit_test "Apply Chaining"
+   K3.SR.string_of_expr
+   (K3Optimizer.optimize ["NATIONKEY"] buggy_apply_opt)
+   (
+   K3.SR.Iterate(
+      (K3.SR.Lambda(
+         K3.SR.ATuple(["QUERY_1_1NATION_CN__NATIONKEY",(K3.SR.TFloat); 
+                       "QUERY_1_1_pNATION1ORDERS_ORDERS__ORDERKEY",
+                           (K3.SR.TFloat);
+                       "TYPE",(K3.SR.TFloat); 
+                       "updated_v",(K3.SR.TFloat) ]),
+         (K3.SR.PCValueUpdate(
+            (K3.SR.PC(
+               "QUERY_1_1_pNATION1_pORDERS1_pCUSTOMER1",
+               [ "NATIONKEY",(K3.SR.TFloat) ],
+               ["QUERY_1_1NATION_CN__NATIONKEY",TFloat;
+                        "QUERY_1_1_pNATION1ORDERS_ORDERS__ORDERKEY",TFloat;
+                        "TYPE",TFloat],
+               (K3.SR.TFloat))),
+            [ (K3.SR.Var("NATIONKEY",(K3.SR.TFloat))) ],
+            [ (K3.SR.Var("QUERY_1_1NATION_CN__NATIONKEY",(K3.SR.TFloat)));
+              (K3.SR.Var("QUERY_1_1_pNATION1ORDERS_ORDERS__ORDERKEY",(K3.SR.TFloat))); 
+              (K3.SR.Var("TYPE",(K3.SR.TFloat))) ],
+            (K3.SR.Var("updated_v",(K3.SR.TFloat))))))
+      ),
+      Lookup(
+    PC("QUERY_1_1_pNATION1_pORDERS1_pCUSTOMER1",["NATIONKEY",TFloat],["QUERY_1_1NATION_CN__NATIONKEY",TFloat;"QUERY_1_1_pNATION1ORDERS_ORDERS__ORDERKEY",TFloat;"TYPE",TFloat],TFloat),
+    [Var("NATIONKEY",TFloat);
+    ])
+   ))
+;;
