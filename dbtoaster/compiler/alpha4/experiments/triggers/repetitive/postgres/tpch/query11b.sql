b071881d422435d44d4bd1535a9bb3b29fd83454
sykora <sykora@dbtoaster.org>
2011-10-06 21:05:42 +0000

    Add postgres implementation of tpch/query11b for repetitive triggers.
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@1492 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: b70dc77fe1761ee4f76c68b09ac6b590b5c9f974

---
+++ dbtoaster/compiler/alpha4/experiments/triggers/repetitive/postgres/tpch/query11b.sql
@@ -0,0 +1,75 @@
+DROP TABLE IF EXISTS PARTSUPP;
+CREATE TABLE PARTSUPP (LIKE PARTSUPP_STREAM);
+
+DROP TABLE IF EXISTS SUPPLIER;
+CREATE TABLE SUPPLIER (LIKE SUPPLIER_STREAM);
+
+DROP TABLE IF EXISTS RESULTS;
+CREATE TABLE RESULTS (total double precision);
+
+CREATE OR REPLACE FUNCTION on_insert_partsuppf() RETURNS TRIGGER AS $on_insert_partsuppf$
+    DECLARE
+        item RESULTS%ROWTYPE;
+    BEGIN
+        FOR item IN
+          SELECT case when sum(ps.supplycost * ps.availqty) is null then 0 else sum(ps.supplycost * ps.availqty) end
+          FROM PARTSUPP ps, SUPPLIER s
+          WHERE ps.suppkey = s.suppkey
+        LOOP
+            UPDATE RESULTS SET total = total + item.total;
+
+            IF found THEN
+                CONTINUE;
+            END IF;
+
+            INSERT INTO RESULTS VALUES (item.total);
+        END LOOP;
+
+        RETURN new;
+    END;
+$on_insert_partsuppf$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION on_insert_supplierf() RETURNS TRIGGER AS $on_insert_supplierf$
+    DECLARE
+        item RESULTS%ROWTYPE;
+    BEGIN
+        FOR item IN
+          SELECT
+            case when
+                sum(ps.supplycost * ps.availqty) is null then 0
+                else sum(ps.supplycost * ps.availqty)
+            end
+          FROM PARTSUPP ps, SUPPLIER s
+          WHERE ps.suppkey = s.suppkey
+        LOOP
+            UPDATE RESULTS SET total = total + item.total;
+
+            IF found THEN
+                CONTINUE;
+            END IF;
+
+            INSERT INTO RESULTS VALUES (item.total);
+        END LOOP;
+
+        RETURN new;
+    END;
+$on_insert_supplierf$ LANGUAGE plpgsql;
+
+CREATE TRIGGER on_insert_partsupp AFTER INSERT ON PARTSUPP
+    FOR EACH ROW EXECUTE PROCEDURE on_insert_partsuppf();
+
+CREATE TRIGGER on_insert_supplier AFTER INSERT ON SUPPLIER
+    FOR EACH ROW EXECUTE PROCEDURE on_insert_supplierf();
+
+INSERT INTO PARTSUPP SELECT * FROM PARTSUPP_STREAM;
+INSERT INTO SUPPLIER SELECT * FROM SUPPLIER_STREAM;
+
+SELECT * FROM RESULTS;
+
+DROP TRIGGER on_insert_partsupp ON PARTSUPP;
+DROP TRIGGER on_insert_supplier ON SUPPLIER;
+
+DROP TABLE RESULTS;
+
+DROP TABLE PARTSUPP;
+DROP TABLE SUPPLIER;
