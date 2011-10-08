ba2d78b5cbf69744948ff6ff869e4a59952b7444
sykora <sykora@dbtoaster.org>
2011-10-08 22:36:38 +0000

    Modify tpch/query11a to allow for deletes.
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@1510 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: b28973b50eb1b6d49df874d4edfd17c8cbfa22ba

--- dbtoaster/compiler/alpha4/experiments/triggers/repetitive/postgres/tpch/query11a.sql
+++ dbtoaster/compiler/alpha4/experiments/triggers/repetitive/postgres/tpch/query11a.sql
@@ -1,8 +1,37 @@
+-- Needs a multiplexed input over all relations in the query to produce results, since otherwise
+-- there will never be any join.
+
+DROP TABLE IF EXISTS PARTSUPP_STREAM;
+CREATE TABLE PARTSUPP_STREAM (
+    stage            integer,
+    flag             integer,
+    partkey          integer,
+    suppkey          integer,
+    availqty         integer,
+    supplycost       double precision,
+    comment          character varying (199)
+);
+
+DROP TABLE IF EXISTS SUPPLIER_STREAM;
+CREATE TABLE SUPPLIER_STREAM (
+    stage            integer,
+    flag             integer,
+    suppkey          integer,
+    name             character (25),
+    address          character varying (40),
+    nationkey        integer,
+    phone            character (15),
+    acctbal          double precision,
+    comment          character varying (101)
+);
+
 DROP TABLE IF EXISTS PARTSUPP;
 CREATE TABLE PARTSUPP (LIKE PARTSUPP_STREAM);
+ALTER TABLE PARTSUPP DROP COLUMN stage, DROP COLUMN flag;
 
 DROP TABLE IF EXISTS SUPPLIER;
 CREATE TABLE SUPPLIER (LIKE SUPPLIER_STREAM);
+ALTER TABLE SUPPLIER DROP COLUMN stage, DROP COLUMN flag;
 
 DROP TABLE IF EXISTS RESULTS;
 CREATE TABLE RESULTS (
@@ -64,21 +93,64 @@ CREATE OR REPLACE FUNCTION on_insert_supplierf() RETURNS TRIGGER AS $on_insert_s
     END;
 $on_insert_supplierf$ LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION on_insert_partsupp_streamf() RETURNS TRIGGER AS $on_insert_partsupp_streamf$
+    BEGIN
+        IF new.flag = 1 THEN
+            INSERT INTO PARTSUPP VALUES (new.partkey, new.suppkey, new.availqty, new.supplycost, new.comment);
+        ELSE
+            DELETE FROM PARTSUPP WHERE partkey = new.partkey AND suppkey = new.suppkey;
+        END IF;
+
+        RETURN new;
+    END;
+$on_insert_partsupp_streamf$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION on_insert_supplier_streamf() RETURNS TRIGGER AS $on_insert_supplier_streamf$
+    BEGIN
+        IF new.flag = 1 THEN
+            INSERT INTO SUPPLIER VALUES (new.suppkey, new.name, new.address, new.nationkey, new.phone, new.acctbal, new.comment);
+        ELSE
+            DELETE FROM SUPPLIER WHERE suppkey = new.suppkey;
+        END IF;
+
+        RETURN new;
+    END;
+$on_insert_supplier_streamf$ LANGUAGE plpgsql;
+
+CREATE TRIGGER on_insert_partsupp_stream AFTER INSERT ON PARTSUPP_STREAM
+    FOR EACH ROW EXECUTE PROCEDURE on_insert_partsupp_streamf();
+
+CREATE TRIGGER on_insert_supplier_stream AFTER INSERT ON SUPPLIER_STREAM
+    FOR EACH ROW EXECUTE PROCEDURE on_insert_supplier_streamf();
+
 CREATE TRIGGER on_insert_partsupp AFTER INSERT ON PARTSUPP
     FOR EACH ROW EXECUTE PROCEDURE on_insert_partsuppf();
 
 CREATE TRIGGER on_insert_supplier AFTER INSERT ON SUPPLIER
     FOR EACH ROW EXECUTE PROCEDURE on_insert_supplierf();
 
-INSERT INTO PARTSUPP SELECT * FROM PARTSUPP_STREAM;
-INSERT INTO SUPPLIER SELECT * FROM SUPPLIER_STREAM;
+CREATE TRIGGER on_delete_partsupp AFTER DELETE ON PARTSUPP
+    FOR EACH ROW EXECUTE PROCEDURE on_insert_partsuppf();
+
+CREATE TRIGGER on_delete_supplier AFTER DELETE ON SUPPLIER
+    FOR EACH ROW EXECUTE PROCEDURE on_insert_supplierf();
+
+COPY PARTSUPP_STREAM FROM '/common/dbtoaster/data/tpch/tpch_tiny_w_del/partsupp.tbl' WITH DELIMITER '|';
+COPY SUPPLIER_STREAM FROM '/common/dbtoaster/data/tpch/tpch_tiny_w_del/supplier.tbl' WITH DELIMITER '|';
 
 SELECT * FROM RESULTS;
 
 DROP TRIGGER on_insert_partsupp ON PARTSUPP;
 DROP TRIGGER on_insert_supplier ON SUPPLIER;
+DROP TRIGGER on_delete_partsupp ON PARTSUPP;
+DROP TRIGGER on_delete_supplier ON SUPPLIER;
+DROP TRIGGER on_insert_partsupp_stream ON PARTSUPP_STREAM;
+DROP TRIGGER on_insert_supplier_stream ON SUPPLIER_STREAM;
 
 DROP TABLE RESULTS;
 
 DROP TABLE PARTSUPP;
 DROP TABLE SUPPLIER;
+
+DROP TABLE PARTSUPP_STREAM;
+DROP TABLE SUPPLIER_STREAM;
