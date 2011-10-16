0a0530f0063f46c8e8b75ab07c142cefe3984397
Yanif Ahmad <yanif.ahmad@gmail.com>
2011-10-16 23:05:55 +0000

    Automated streambase experiments.
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@1554 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 6dc8821142b6d016b43e211f340e7fc6afcf05f3

---
+++ dbtoaster/compiler/alpha4/experiments/streams/streambase/scripts/run_spe.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+
+import os.path, shutil, subprocess, time
+from csv import DictReader
+from sys import argv
+from optparse import OptionParser
+
+default_dir = "/damsl/software/streambase"
+event_file_name = "file_events.csv"
+
+queries = {
+  'query3' : ['lineitem', 'orders', 'customer'], 
+  'query17': ['lineitem', 'part'],
+  'query18': ['lineitem', 'orders', 'customer'],
+  'query22': ['orders', 'customer']
+}
+
+num_files = {}
+for qn in queries:
+  num_files[qn] = len(queries[qn])
+
+# Check if SPE's CSV adaptor is done with all files for the given query.
+#####
+def check_terminate(query_name):
+  done = False
+  try:
+    with open(event_file_name) as evt_file:
+      reader = DictReader(evt_file)
+      files_closed = 0
+      for row in reader:
+        if row['Type'] == 'Close':
+          cf = os.path.splitext(os.path.basename(row['Object']))[0].lower()
+          if query_name in queries and cf in queries[query_name]:
+            files_closed += 1
+      done = files_closed == num_files[query_name]
+  except IOError:
+    print event_file_name+" not found... continuing to poll"
+  return done
+
+# Called after termination to save any experiment outputs
+def save_run(query_name):
+  if os.path.exists(event_file_name):
+    shutil.move(event_file_name, query_name+"_"+event_file_name)
+
+# Run SPE in a subprocess, poll for termination based on file events,
+# send SIGTERM to terminate the SPE
+#####
+def run_stream_engine(sbdir, query_name, poll_period):
+  terminate = False
+  loader = query_name+"_separate_loader.ssql"
+  
+  # Spawn and poll.
+  spe = subprocess.Popen([os.path.join(sbdir, "bin/sbd"), loader])
+  while not(terminate):
+    time.sleep(poll_period)
+    done = spe.poll() 
+    terminate = check_terminate(query_name) or (done != None)
+  
+  # Cleanup.
+  print "SPE done, terminating..."
+  if spe.poll() == None:
+    spe.terminate()
+  save_run(query_name)
+
+if __name__ == '__main__':
+  usage = "Usage: %prog [options] <query name>"
+  parser = OptionParser(usage=usage)
+  parser.add_option("-p", "--poll", type="int", dest="poll_period", default=10,
+                    help="set termination polling period", metavar="SECS")
+  parser.add_option("-d", "--sbdir", dest="sbdir",
+                    default=default_dir,
+                    help="set SPE top-level dir", metavar="DIR")
+                    
+  (options, args) = parser.parse_args()
+  
+  if len(args) < 1:
+    parser.error("no query specified")
+  
+  # Check SPE binaries  
+  test_sb_paths = [options.sbdir, os.path.join(options.sbdir, "bin/sbd")]
+  if not(all([os.path.exists(p) for p in test_sb_paths])):
+    parser.error("invalid SPE directory: "+options.sbdir)
+
+  query_name = args[0]
+  poll_period = options.poll_period
+
+  # Check query files
+  test_q_paths = [query_name+".ssql", query_name+"_separate_loader.ssql"]
+  if query_name in queries and all([os.path.exists(p) for p in test_q_paths]):
+    print "Running query " + query_name + " from " + os.getcwd()
+    run_stream_engine(options.sbdir, query_name, poll_period)
+  else:
+    print "Unknown query: " + query_name
+    print "Valid queries: " + str(queries.keys())
+  
---
+++ dbtoaster/compiler/alpha4/experiments/streams/streambase/scripts/run_tpch_experiments.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]; then
+  echo "Usage: $0 <query template dir> <output dir> <data dir>"
+  exit 1
+fi
+
+query_template_dir=$1
+output_dir=$2
+data_dir=$3
+
+working_dir=`pwd`
+script_dir=/home/yna/sbworkspace/tpch/scripts
+
+queries=`ls -1 $query_template_dir/*_separate_loader.ssql | sed 's/_separate_loader.ssql//'`
+
+for i in 0; do
+  # Prepare query files in a new run dir
+  run_dir="$output_dir/run$i"
+  test -d $run_dir || (echo "Creating run output: $run_dir" && mkdir -p $run_dir)
+  
+  echo "Linking dataset..."
+  ln -s $data_dir $run_dir/data
+
+  for q in $queries; do
+    query=`basename $q`
+    if [ -f $query_template_dir/"$query"_separate_loader.ssql ]; then
+      echo "Generating $query.ssql..."
+      sed "s/@@PATH@@/data\//" < $query_template_dir/"$query"_separate_loader.ssql \
+        > $run_dir/"$query"_separate_loader.ssql
+      cp $query_template_dir/"$query".ssql $run_dir/"$query".ssql
+    
+      # Run the SPE on this query.
+      cd $run_dir
+      echo "Running SPE on $query"
+      $script_dir/run_spe.py $query
+      
+      cd $working_dir
+    fi
+  done
+done
