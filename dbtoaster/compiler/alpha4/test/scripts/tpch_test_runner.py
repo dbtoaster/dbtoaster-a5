f471e4fe02b1cb2c4977f0b31b0928b695be9c0c
sykora <sykora@dbtoaster.org>
2011-10-17 17:28:04 +0000

    Add timing capability to tpch test runner.
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@1557 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: c1d5bb60c396ac0697b03112b64f1112e24ea65c

--- dbtoaster/compiler/alpha4/test/scripts/tpch_test_runner.py
+++ dbtoaster/compiler/alpha4/test/scripts/tpch_test_runner.py
@@ -1,22 +1,36 @@
 import argparse
-import signal
 import subprocess
 import time
+import re
+
+progress = re.compile(r'NOTICE:\s+(OPERATION.*)$')
 
 def create_database(database):
     '''Create a new database for testing in.'''
 
-    subprocess.call(['createdb', database])
+    subprocess.call(
+        ['createdb', database],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
 
 def drop_database(database):
     '''Drop our testing database.'''
 
-    subprocess.call(['dropdb', database])
+    subprocess.call(
+        ['dropdb', database],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
 
 def initialize_schema(database):
     '''Initializes our test database.'''
 
-    subprocess.call(['psql', '-f' 'schema_definition.postgres.sql', database])
+    subprocess.call(
+        ['psql', '-f' 'schema_definition.postgres.sql', database],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
 
 def load_agenda(database, agenda):
     '''Loads the supplied agenda into our database.
@@ -27,11 +41,13 @@ def load_agenda(database, agenda):
     database_load = subprocess.Popen(
         ['psql', database],
         stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
     )
 
     database_load.communicate("\copy AGENDA FROM '{}' CSV;".format(agenda).encode())
 
-def run_query(database, query):
+def run_query(database, query, timeout):
     '''Runs our supplied query against our test database.'''
 
     query_results = subprocess.Popen(
@@ -40,9 +56,17 @@ def run_query(database, query):
         stdout=subprocess.PIPE,
     )
 
-    output, error = query_results.communicate()
+    time.sleep(timeout)
+    query_results.kill()
+    error = query_results.stderr.read()
+    terminate_query = subprocess.call(
+        ['psql', '-c', "select pg_cancel_backend(procpid) from pg_stat_activity where current_query LIKE 'SELECT dispatch();'", DATABASE],
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+    )
+
     operations = list(filter(lambda line: 'OPERATION' in line, error.decode().split('\n')))
-    print(operations[-1])
+    print(progress.search(operations[-1]).group(1))
 
 
 if __name__ == '__main__':
@@ -63,16 +87,20 @@ if __name__ == '__main__':
     parser.add_argument(
         'time',
         help='Time (in minutes) to run this query for, before killing it.',
+        type=int,
+    )
+
+    parser.add_argument(
+        '-d', '--debug',
+        help='Print debugging information from the database.',
+        action='store_true',
+        dest='debug',
     )
 
     args = parser.parse_args()
 
     DATABASE = 'dbtoaster'
 
-    create_database(DATABASE)
-    try:
-        initialize_schema(DATABASE)
-        load_agenda(DATABASE, args.agenda)
-        run_query(DATABASE, args.query)
-    finally:
-        drop_database(DATABASE)
+    initialize_schema(DATABASE)
+    load_agenda(DATABASE, args.agenda)
+    run_query(DATABASE, args.query, args.time)
