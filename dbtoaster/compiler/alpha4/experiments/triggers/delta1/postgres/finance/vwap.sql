b1a4b5a28f292769b63119b50a4f9b72b9c15170
sykora <sykora@dbtoaster.org>
2011-10-20 21:43:48 +0000

    Add initial template of working delta1 postgres - vwap.
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@1583 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: a16a1b82286e9fb24f9bccd174b55e61c809a67a

--- dbtoaster/compiler/alpha4/experiments/triggers/delta1/postgres/finance/vwap.sql
+++ dbtoaster/compiler/alpha4/experiments/triggers/delta1/postgres/finance/vwap.sql
@@ -1,180 +1,97 @@
-DROP TABLE IF EXISTS InsertBids;
-CREATE TABLE InsertBids(t float, order_id float, broker_id float, volume float, price float);
+TRUNCATE TABLE ASKS;
+TRUNCATE TABLE BIDS;
 
-DROP TABLE IF EXISTS DeleteBids;
-CREATE TABLE DeleteBids(t float, order_id float, broker_id float, volume float, price float);
+DROP TABLE IF EXISTS RESULTS;
+CREATE TABLE RESULTS (
+    total float
+);
 
-DROP TABLE IF EXISTS Bids;
-CREATE TABLE Bids(volume float, price float);
+INSERT INTO RESULTS VALUES (0);
 
-DROP TABLE IF EXISTS Result;
-CREATE TABLE Result(q float);
-INSERT INTO Result VALUES (0.0);
+CREATE OR REPLACE FUNCTION on_insert_bidsf() RETURNS TRIGGER AS $on_insert_bidsf$
 
-CREATE FUNCTION on_insert_bids_s1f() returns trigger AS $on_insert_bids_s1f$
-  DECLARE
-    v1 float;
-    v2 float;
-    v3 float;
-  BEGIN
-    -- ((if QUERY_BS1_2[QUERYBIDS_B1__PRICE]<(0.25*QUERY_BS1_3[])
-    --   then QUERYBIDS_B1__PRICE else 0)*QUERYBIDS_B1__VOLUME)
+    BEGIN
 
-	v1 :=
-    ( select new.price * new.volume where
-        (select case when v is null then 0 else v end
-         from (select sum(b2.volume) as v from bids b2
-               where b2.price > new.price) as query_bs1_2)
-        < (0.25*(select case when sum(b3.volume)
-                        is null then 0 else sum(b3.volume) end
-                 from bids b3)) );
+        UPDATE RESULTS SET total = total +
+            (SELECT new.volume*new.price
+            WHERE (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS WHERE price > new.price) < 0.25 * (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS))
 
-    -- (if (QUERY_BS1_2[QUERYBIDS_B1__PRICE]+
-    --     (QUERYBIDS_B1__VOLUME*(if QUERYBIDS_B1__PRICE<QUERYBIDS_B1__PRICE then 1 else 0))) < 
-    --     ((0.25*QUERY_BS1_3[])+(0.25*QUERYBIDS_B1__VOLUME))
-    --     and (0.25*QUERY_BS1_3[])<=QUERY_BS1_2[QUERYBIDS_B1__PRICE]
-    --  then (QUERYBIDS_B1__PRICE*QUERYBIDS_B1__VOLUME) else 0)
+          + (SELECT new.volume*new.price
+            WHERE ((SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS WHERE price > new.price) < 0.25 * (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS) + 0.25 * new.volume)
+            AND 0.25 * (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS) <= (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS WHERE price > new.price))
 
-    v2 :=
-    ( select new.price * new.volume where
-        ( (select case when v is null then 0 else v end
-           from (select sum(b2.volume) as v from bids b2
-               where b2.price > new.price) as query_bs1_2)
-          < ((0.25*(select case when sum(b3.volume)
-                           is null then 0 else sum(b3.volume) end
-                    from bids b3))
-             +(0.25*new.volume)) )
-        and
-        ((0.25*(select case when sum(b3.volume)
-                       is null then 0 else sum(b3.volume) end
-                from bids b3))
-         <=
-          (select case when v is null then 0 else v end
-             from (select sum(b2.volume) as v from bids b2
-               where b2.price > new.price) as query_bs1_2)) );
+          - (SELECT new.volume*new.price
+            WHERE (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS WHERE price > new.price) < 0.25 * (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS)
+            AND (0.25 * (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS) + 0.25 * new.volume) <= (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS WHERE price > new.price))
 
-    -- ((if QUERY_BS1_2[QUERYBIDS_B1__PRICE]<(0.25*QUERY_BS1_3[])
-    --      and ((0.25*QUERY_BS1_3[])+(0.25*QUERYBIDS_B1__VOLUME)) <=
-    --           (QUERY_BS1_2[QUERYBIDS_B1__PRICE]+
-    --           (QUERYBIDS_B1__VOLUME*(if QUERYBIDS_B1__PRICE<QUERYBIDS_B1__PRICE then 1 else 0)))
-    --   then (QUERYBIDS_B1__PRICE*QUERYBIDS_B1__VOLUME) else 0)*-1)
+          + (SELECT (CASE WHEN SUM(sub.total) IS NULL THEN 0 ELSE SUM(sub.total) END)
+            FROM (
+                SELECT price, sum(price * volume) as total
+                FROM BIDS
+                GROUP BY price
+                HAVING ((SELECT (CASE WHEN SUM(VOLUME) IS NULL THEN 0 ELSE SUM(VOLUME) END) FROM BIDS b WHERE b.price > price) + (case when price < new.price then new.volume else 0 end)) < (0.25 * (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS) + 0.25 * new.volume)
+                AND 0.25 * (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS) <= (SELECT (CASE WHEN SUM(VOLUME) IS NULL THEN 0 ELSE SUM(VOLUME) END) FROM BIDS b WHERE b.price > price)
+            ) AS sub)
 
-     v3 :=
-     ( select -1 * (new.price * new.volume) where
-         ( ( (select case when v is null then 0 else v end        
-              from (select sum(b2.volume) as v from bids b2
-                    where b2.price > new.price) as query_bs1_2)
-           < (0.25*(select case when sum(b3.volume)
-                           is null then 0 else sum(b3.volume) end  
-                    from bids b3)) )
-         and
-           ( ((0.25*(select case when sum(b3.volume)
-                            is null then 0 else sum(b3.volume) end 
-                     from bids b3))
-              +(0.25*new.volume))
-             <= (select case when v is null then 0 else v end
-                 from (select sum(b2.volume) as v from bids b2
-                       where b2.price > new.price) as query_bs1_2) ) ) );
+          - (SELECT (CASE WHEN SUM(sub.total) IS NULL THEN 0 ELSE SUM(sub.total) END)
+            FROM (
+                SELECT price, sum(price * volume) as total
+                FROM BIDS
+                GROUP BY price
+                HAVING ((SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS b WHERE b.price > price) < (0.25 * (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS)))
+                AND (0.25 * (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS) + 0.25 * new.volume) <= ((SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS b WHERE b.price > price) + (case when price < new.price then new.volume else 0 end))
+            ) AS sub);
 
-    update result set q = q +
-      (select case when v1 is null then 0 else v1 end) +
-      (select case when v2 is null then 0 else v2 end) +
-      (select case when v3 is null then 0 else v3 end);
-	  
-    return new;
-  END;
-$on_insert_bids_s1f$ LANGUAGE plpgsql;
+        return new;
+    END;
+$on_insert_bidsf$ LANGUAGE plpgsql;
 
-CREATE FUNCTION on_insert_bids_s2f() returns trigger AS $on_insert_bids_s2f$
-  DECLARE
-    v1 float;
-    v2 float;
-  BEGIN
-    -- (if (QUERY_BS1_2[B1__PRICE]+
-    --     (QUERYBIDS_B1__VOLUME*(if B1__PRICE<QUERYBIDS_B1__PRICE then 1 else 0)))
-    --      < ((0.25*QUERY_BS1_3[])+(0.25*QUERYBIDS_B1__VOLUME))
-    --    and (0.25*QUERY_BS1_3[])<=QUERY_BS1_2[B1__PRICE]
-    --    then QUERY_BS1_1[B1__PRICE] else 0)
+CREATE OR REPLACE FUNCTION on_delete_bidsf() RETURNS TRIGGER AS $on_delete_bidsf$
+    BEGIN
+        UPDATE RESULTS SET total = total -
+            (SELECT old.volume*old.price
+            WHERE (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS WHERE price > old.price) < 0.25 * (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS))
 
-    v1 :=
-    ( select sum(b1.price*b1.volume) from bids b1
-      where ( ((select case when v is null then 0 else v end
-                from (select sum(b2.volume) as v from bids b2
-                      where b2.price > b1.price) as query_bs1_2)
-               + (select case when b1.price < new.price
-                         then new.volume else 0 end))
-              < ((0.25*(select case when sum(b3.volume)
-                           is null then 0 else sum(b3.volume) end
-                        from bids b3))
-                 +(0.25*new.volume))
-             and
-             (0.25*(select case when sum(b3.volume)
-                           is null then 0 else sum(b3.volume) end
-                    from bids b3)
-             <= (select case when v is null then 0 else v end
-                 from (select sum(b2.volume) as v from bids b2
-                       where b2.price > b1.price) as query_bs1_2)) ) );
+          - (SELECT old.volume*old.price
+            WHERE ((SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS WHERE price > old.price) < 0.25 * (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS) - 0.25 * old.volume)
+            AND 0.25 * (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS) <= (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS WHERE price > old.price))
 
-    -- ((if  QUERY_BS1_2[B1__PRICE]<(0.25*QUERY_BS1_3[])
-    --     and ((0.25*QUERY_BS1_3[])+(0.25*QUERYBIDS_B1__VOLUME)) <= 
-    --          (QUERY_BS1_2[B1__PRICE]+
-    --          (QUERYBIDS_B1__VOLUME*(if B1__PRICE<QUERYBIDS_B1__PRICE then 1 else 0)))
-    --     then QUERY_BS1_1[B1__PRICE] else 0)*-1)
+          + (SELECT old.volume*old.price
+            WHERE (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS WHERE price > old.price) < 0.25 * (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS)
+            AND (0.25 * (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS) - 0.25 * old.volume) <= (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS WHERE price > old.price))
 
-    v2 :=
-    ( select -1*sum(b1.price*b1.volume) from bids b1
-      where ( ((select case when v is null then 0 else v end
-                from (select sum(b2.volume) as v from bids b2
-                      where b2.price > b1.price) as query_bs1_2)
-               < (0.25*(select case when sum(b3.volume)
-                               is null then 0 else sum(b3.volume) end
-                        from bids b3)) )
-              and
-              (((0.25*(select case when sum(b3.volume)
-                              is null then 0 else sum(b3.volume) end
-                        from bids b3))
-                 + (0.25*new.volume))
-               <= ((select case when v is null then 0 else v end
-                    from (select sum(b2.volume) as v from bids b2
-                          where b2.price > b1.price) as query_bs1_2)
-                   + (select case when b1.price < new.price
-                             then new.volume else 0 end))) ) );
+          + (SELECT (CASE WHEN SUM(sub.total) IS NULL THEN 0 ELSE SUM(sub.total) END)
+            FROM (
+                SELECT price, sum(price * volume) as total
+                FROM BIDS
+                GROUP BY price
+                HAVING ((SELECT (CASE WHEN SUM(VOLUME) IS NULL THEN 0 ELSE SUM(VOLUME) END) FROM BIDS b WHERE b.price > price) - (case when price < old.price then old.volume else 0 end)) < (0.25 * (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS) - 0.25 * old.volume)
+                AND 0.25 * (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS) <= (SELECT (CASE WHEN SUM(VOLUME) IS NULL THEN 0 ELSE SUM(VOLUME) END) FROM BIDS b WHERE b.price > price)
+            ) AS sub)
 
-    update result set q = q +
-      (select case when v1 is null then 0 else v1 end) +
-      (select case when v2 is null then 0 else v2 end);
+          - (SELECT (CASE WHEN SUM(sub.total) IS NULL THEN 0 ELSE SUM(sub.total) END)
+            FROM (
+                SELECT price, sum(price * volume) as total
+                FROM BIDS
+                GROUP BY price
+                HAVING ((SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS b WHERE b.price > price) < (0.25 * (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS)))
+                AND (0.25 * (SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS) - 0.25 * old.volume) <= ((SELECT (CASE WHEN SUM(volume) IS NULL THEN 0 ELSE SUM(volume) END) FROM BIDS b WHERE b.price > price) - (case when price < old.price then old.volume else 0 end))
+            ) AS sub);
 
-    return new;
-  END;
-$on_insert_bids_s2f$ LANGUAGE plpgsql;
+        return old;
+    END;
+$on_delete_bidsf$ LANGUAGE plpgsql;
 
--- Triggers must be executed before insert to Bids to allow use of old value
--- of Bids in SQL queries.
-CREATE TRIGGER on_insert_bids_s1 BEFORE INSERT ON Bids
-    FOR EACH ROW EXECUTE PROCEDURE on_insert_bids_s1f();
+CREATE TRIGGER on_insert_bids AFTER INSERT ON BIDS
+    FOR EACH ROW EXECUTE PROCEDURE on_insert_bidsf();
 
-CREATE TRIGGER on_insert_bids_s2 BEFORE INSERT ON Bids
-    FOR EACH ROW EXECUTE PROCEDURE on_insert_bids_s2f();
+CREATE TRIGGER on_delete_bids AFTER delete ON BIDS
+    FOR EACH ROW EXECUTE PROCEDURE on_delete_bidsf();
 
-    
-COPY InsertBids FROM '@@PATH@@/testdata/InsertBIDS.dbtdat' WITH DELIMITER ',';
-COPY DeleteBids FROM '@@PATH@@/testdata/DeleteBIDS.dbtdat' WITH DELIMITER ',';
+SELECT dispatch();
 
-INSERT INTO Bids (price, volume)
-  SELECT price, volume FROM InsertBids
-  EXCEPT ALL SELECT price, volume FROM DeleteBids;
+SELECT * FROM RESULTS;
 
-SELECT q FROM Result;
-
--- Cleanup
-DROP TRIGGER on_insert_bids_s2 ON Bids;
-DROP TRIGGER on_insert_bids_s1 ON Bids;
-
-DROP FUNCTION on_insert_bids_s2f();
-DROP FUNCTION on_insert_bids_s1f();
-
-DROP TABLE Result;
-DROP TABLE Bids;
-
-DROP TABLE InsertBids;
-DROP TABLE DeleteBids;
+DROP TABLE RESULTS;
+DROP TRIGGER on_insert_bids ON BIDS;
+DROP TRIGGER on_delete_bids ON BIDS;
