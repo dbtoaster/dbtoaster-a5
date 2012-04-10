23adbaf363c1812f4f44acad6b9a75f7caf34bd7
Milos Nikolic <Milos.Nikolic@epfl.ch>
2012-04-10 15:57:12 +0000

    Added TPCH-18; fails to compile into M3
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@1969 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 2c7280b5aba96a72abebccfc2640e14d7c53298f

---
+++ test/queries/tpch/query18.sql
@@ -0,0 +1,58 @@
+CREATE STREAM LINEITEM (
+        orderkey       INT,
+--        partkey        INT,
+--        suppkey        INT,
+--        linenumber     INT,
+        quantity       DECIMAL
+--        extendedprice  DECIMAL,
+--        discount       DECIMAL,
+--        tax            DECIMAL,
+--        returnflag     CHAR(1),
+--        linestatus     CHAR(1),
+--        shipdate       DATE,
+--        commitdate     DATE,
+--        receiptdate    DATE,
+--        shipinstruct   CHAR(25),
+--        shipmode       CHAR(10),
+--        comment        VARCHAR(44)
+    )
+  FROM FILE '../../experiments/data/tpch/lineitem.csv'
+  LINE DELIMITED csv (deletions := 'true');
+
+CREATE STREAM ORDERS (
+        orderkey       INT,
+        custkey        INT
+--        orderstatus    CHAR(1),
+--        totalprice     DECIMAL,
+--        orderdate      DATE,
+--        orderpriority  CHAR(15),
+--        clerk          CHAR(15),
+--        shippriority   INT,
+--        comment        VARCHAR(79)
+    )
+  FROM FILE '../../experiments/data/tpch/orders.csv'
+  LINE DELIMITED csv (deletions := 'true');
+
+CREATE STREAM CUSTOMER (
+        custkey      INT
+--        name         VARCHAR(25),
+--        address      VARCHAR(40),
+--        nationkey    INT,
+--        phone        CHAR(15),
+--        acctbal      DECIMAL,
+--        mktsegment   CHAR(10),
+--        comment      VARCHAR(117)
+    )
+  FROM FILE '../../experiments/data/tpch/customer.csv'
+  LINE DELIMITED csv (deletions := 'true');
+
+SELECT c.custkey, sum(l1.quantity) AS query18
+FROM customer c, orders o, lineitem l1
+WHERE 1 <=
+      (SELECT sum(1) FROM lineitem l2
+       WHERE l1.orderkey = l2.orderkey
+       AND 100 < (SELECT sum(l3.quantity) FROM lineitem l3
+                  WHERE l2.orderkey = l3.orderkey))
+AND c.custkey = o.custkey
+AND o.orderkey = l1.orderkey
+GROUP BY c.custkey;
