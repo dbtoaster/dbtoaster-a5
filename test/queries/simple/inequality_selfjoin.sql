e29cf36b3ed145dd297ef759f274303c6088aad5
Oliver Kennedy <okennedy@buffalo.edu>
2012-06-07 08:58:02 +0000

    Query for (re #173)
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@2245 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 4b0c0762c5beac9ee825a90c423d2b3f4eebf0a0

---
+++ test/queries/simple/inequality_selfjoin.sql
@@ -0,0 +1,6 @@
+CREATE STREAM R(A int, B int)
+FROM FILE '../../experiments/data/tiny_r.dat' LINE DELIMITED
+csv (fields := ',', schema := 'int,int', eventtype := 'insert');
+
+SELECT r1.A FROM R r1, R r2 WHERE NOT (R1.A=R2.A);
+--SELECT r1.A FROM R r1, R r2 WHERE R1.A != R2.A;
