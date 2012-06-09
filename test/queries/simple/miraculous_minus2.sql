e80991db4aefb30cbd485f752e37193b1293e0c2
Oliver Kennedy <okennedy@buffalo.edu>
2012-06-09 23:58:59 +0000

    A version miraculous minus that actually produces results
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@2275 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 33a5aab49e8c6f44014491abe8f9190a038c30fa

---
+++ test/queries/simple/miraculous_minus2.sql
@@ -0,0 +1,6 @@
+CREATE STREAM R(A int, B int)
+FROM FILE '../../experiments/data/tiny_r.dat' LINE DELIMITED
+csv (fields := ',', schema := 'int,int', eventtype := 'insert');
+
+SELECT r1.A FROM R r1 WHERE r1.A =
+(SELECT SUM(r2.B)*0.5 FROM R r2 WHERE r2.A = r1.B);
