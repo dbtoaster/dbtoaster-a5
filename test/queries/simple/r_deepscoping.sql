3035fa65ecca8143b721619d77c298f9c4b35caa
Oliver Kennedy <okennedy@buffalo.edu>
2012-06-10 04:42:51 +0000

    Bringing more queries into the lovely unit test fold
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@2283 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: f2bc9b877af18b019dce4670f77d1c213206738b

--- test/queries/simple/r_deepscoping.sql
+++ test/queries/simple/r_deepscoping.sql
@@ -4,6 +4,6 @@ csv (fields := ',', schema := 'int,int', eventtype := 'insert');
 
 SELECT r3.A
 FROM (SELECT r1.A FROM R r1,
-(SELECT r2.A FROM R r2 WHERE r1.A=r2.A) r2b) r3,
+(SELECT r2.A FROM R r2) r2b WHERE r1.A=r2b.A) r3,
 R r4
 WHERE r3.A = r4.A
--- test/queries/simple/r_deletions.sql
+++
@@ -1,3 +0,0 @@
-CREATE STREAM R(A int, B int)
-FROM FILE '../../experiments/data/tiny_r_deletions.dat' LINE DELIMITED
-csv (fields := ',', triggers := '0:delete,1:insert');
