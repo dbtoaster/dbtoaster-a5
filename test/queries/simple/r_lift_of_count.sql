b5d3cbb2b0997b9e689464ace46d761f00ab4353
Daniel Lupei <daniel.lupei@epfl.ch>
2012-06-18 09:20:20 +0000

    Commented the extra query such that query_test.rb doesn't get confused.
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@2315 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 0644651eb15058d7966075de3fc9123a330aafbf

--- test/queries/simple/r_lift_of_count.sql
+++ test/queries/simple/r_lift_of_count.sql
@@ -2,7 +2,7 @@ CREATE STREAM R(A int, B int)
 FROM FILE '../../experiments/data/tiny_r.dat' LINE DELIMITED
 csv ();
 
-SELECT r1.A, COUNT(*) AS C FROM R r1 GROUP BY r1.A;
+--SELECT r1.A, COUNT(*) AS C FROM R r1 GROUP BY r1.A;
 
 SELECT r2.C FROM (
   SELECT r1.A, COUNT(*) AS C FROM R r1 GROUP BY r1.A
