44d34ea00cd52165ef3927e240cb96b909821465
Andres Noetzli <noetzli@dbtoaster.org>
2012-07-04 21:57:42 +0000

    Scala doc
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@2472 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: ab99de8127600de0326213b6dbcfcb67ac4601a2

--- doc/site/docs_scala.php
+++ doc/site/docs_scala.php
@@ -43,8 +43,8 @@ Run time: 0.042 ms
 &lt;RESULT&gt;156 &lt;/RESULT&gt;
 </div>
 
-<a name="generatedcode"/>
-<?=chapter("Generated Code Reference")?>
+<a name="apiguide"/>
+<?= chapter("Scala API Guide") ?>
 The following example shows how a query can be ran from your own Scala code. Suppose we have a the following source code in <tt>main_example.scala</tt>:
 <div class="codeblock">
 import org.dbtoaster.Query
@@ -60,6 +60,12 @@ package org.example {
 </div>
 This program will start the query and output its result after it finished.
 <p>
+To retrieve results, the <tt>get<i>RESULTNAME</i>()</tt> of the <tt>Query</tt> object can be used.
+<p>
+<i>Note:</i>The <tt>get<i>RESULTNAME</i>()</tt> functions are not thread-safe, meaning that results can be 
+inconsistent if they are called from another thread than the query thread. A thread-safe alternative to retrieve
+the results is planned for future versions of DBToaster.
+<p>
 The program can be compiled to <tt>main_example.jar</tt> using the following command (assuming that the query was compiled to a file named <tt>rs_example.jar</tt>):
 <div class="codeblock">
 $&gt; scalac -classpath "rs_example.jar" -d main_example.jar main_example.scala
@@ -67,4 +73,120 @@ $&gt; scalac -classpath "rs_example.jar" -d main_example.jar main_example.scala
 The resulting program can now be launched with:
 <div class="codeblock">
 $&gt; scala -classpath "main_example.jar:rs_example.jar:lib/dbt_scala/dbtlib.jar" org.example.MainExample
+</div>
+The <tt>Query.run()</tt> method takes a function of type <tt>Unit => Unit</tt> as an optional argument which is called every time when an event was processed. 
+This function can be used to retrieve results while the query is still running.
+<p>
+<i>Note:</i> The function will be executed on the same thread on which the query processing takes place, blocking further query processing while the function is being run.
+
+<a name="generatedcode"/>
+<?=chapter("Generated Code Reference")?>
+The DBToaster Scala codegenerator generates a single file containing an object <tt>Query</tt> in the package <tt>org.dbtoaster</tt>.
+<p>
+For the previous example the generated code looks like this:
+<div class="codeblock">
+// Imports
+import java.io.FileInputStream;
+...
+
+package org.dbtoaster {
+  // The generated object
+  object Query {
+    // Declaration of sources
+    val s1 = createInputStreamSource(new FileInputStream("../../experiments/data/simple/tiny/r.dat"), ...);
+    ...
+
+    // Data structures holding the intermediate result
+    var RESULT = SimpleVal[Long](0);
+    ...
+
+    // Functions to retrieve the result
+    def getRESULT():Long = {
+      RESULT.get()
+    };
+
+    // Trigger functions
+    def onInsertR(var_R_A: Long,var_R_B: Long) = ...
+    ...
+    def onDeleteS(var_S_B: Long,var_S_C: Long) = ...
+    
+    // Functions that handle static tables and system initialization
+    def onSystemInitialized() = ...
+    def fillTables(): Unit = ...
+    
+    // Function that dispatches events to the appropriate trigger functions 
+    def dispatcher(event: DBTEvent, onEventProcessedHandler: Unit => Unit): Unit = ...
+    
+    // (Blocking) function to start the execution of the query
+    def run(onEventProcessedHandler: Unit => Unit = (_ => ())): Unit = ...
+    
+    // Prints the query results in some XML-like form (used mainly for debugging purposes) 
+    def printResults(): Unit = ...
+  }
+}
+</div>
+<p>
+When the <tt>run</tt> method is called, the static tables are loaded and the processing
+of events from the declared sources starts. The function returns when the sources provide no
+more events.
+<p>
+
+<?=section("Retrieving results")?>
+<p>
+To retrieve the result, the <tt>get<i>RESULTNAME</i>()</tt> functions are used. In the example above,
+the <tt>get<i>RESULT</i>()</tt> method is simple but more complex methods may be generated
+and the return value may be a collection instead of a single value.
+<p>
+
+<?=subsection("Queries computing collections")?>
+<p>
+Consider the following query:
+<div class="codeblock">
+CREATE STREAM R(A int, B int) 
+  FROM FILE '../../experiments/data/tiny/r.dat' LINE DELIMITED
+  CSV (fields := ',');
+
+CREATE STREAM S(B int, C int) 
+  FROM FILE '../../experiments/data/tiny/s.dat' LINE DELIMITED
+  CSV (fields := ',');
+
+SELECT r.B, SUM(r.A*s.C) as RESULT_1, SUM(r.A+s.C) as RESULT_2 FROM R r, S s WHERE r.B = s.B GROUP BY r.B;
+</div>
+In this case two functions are being generated that can be called to retrieve the result, each of them representing
+one of the result columns:
+<div class="codeblock">
+def getRESULT_1():K3PersistentCollection[(Long), Long] = ...
+def getRESULT_2():K3PersistentCollection[(Long), Long] = ...
+</div>
+In this case, the functions return a collection containing the result. For further processing, the results can be converted 
+to lists of key-value pairs using the <tt>toList()</tt> method of the collection class. The key in the pair corresponds 
+to the columns in the <tt>GROUP BY</tt> clause, in our case <tt>r.B</tt>. The value corresponds to the aggregated 
+value for the corresponding key.
+
+<?=subsection("Partial Materialization")?>
+<p>
+Some of the work involved in maintaining the results of a query can be saved by performing partial materialization
+and only computing the final results when invoking <tt>tlq_t</tt>'s <tt>get_<i>TLQ_NAME</i></tt> functions. This
+behaviour is especially desirable when the rate of querying the results is lower than the rate of updates, and
+can be enabled through the <tt>-F EXPRESSIVE-TLQS</tt> command line flag.
+<br/>
+Below is an example of a query where partial materialization is indeed beneficial.
+<div class="codeblock">
+CREATE STREAM R(A int, B int)
+FROM FILE '../../experiments/data/tiny/r.dat' LINE DELIMITED
+csv ();
+
+SELECT r2.C FROM (
+  SELECT r1.A, COUNT(*) AS C FROM R r1 GROUP BY r1.A
+) r2;
+</div>
+When compiling this query with the <tt>-F EXPRESSIVE-TLQS</tt> command line flag, the function to retrieve
+the results is much more complex, unlike the functions that we have seen before. It uses the partial materialization 
+<tt>COUNT_1_E1_1</tt> to compute the result:
+<div class="codeblock">$&gt; bin/dbtoaster -l scala -F EXPRESSIVE-TLQS test/queries/simple/r_lift_of_count.sql 
+    def getCOUNT():K3IntermediateCollection[(Long), Long] = {
+      (COUNT_1_E1_1.map((y:Tuple2[(Long),Long]) => 
+      ...
+      )
+    };
 </div>
