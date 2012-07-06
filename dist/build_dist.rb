d0b0d92580558873bc48002d5cbbdf39454fde03
Oliver Kennedy <okennedy@buffalo.edu>
2012-07-06 22:36:31 +0000

    Updated dist builder now generates a documentation directory based on the website and includes README and LICENSE files
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@2510 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 58e193547e1ec628103c304bcb59b115584bd6f7

--- dist/build_dist.rb
+++ dist/build_dist.rb
@@ -48,10 +48,68 @@ def fix_sql_file(sql_file, tgt_dir)
   end
 end
 
-copy_files([], "dbtoaster");
+def build_php(script, target_dir)
+  
+  old_dir    = Dir.getwd;
+  script_dir = File.dirname script;
+  
+  Dir.chdir script_dir;
+  
+  sources = Dir.glob("pages/*.php").map do |source|
+      source = File.basename(source, ".php");
+      parts = source.split(/_/);
+      page = parts.shift;
+      subpage = if parts.length > 0 then parts.join("_") else "index" end;
+      [ page, subpage, 
+        if source == "home" then "index.html"
+                            else "#{source}.html" end
+      ];
+    end
+  
+  subs = sources.map do |page, subpage, dest_file|
+    dest_sub = "\"#{dest_file}\"";
+    ret = [ [/"index.php\?page=#{page}&subpage=#{subpage}"/, dest_sub
+             ], 
+             [/"index.php\?page=#{page}&subpage=#{subpage}(\#[a-zA-Z0-9]+)"/, 
+             "\"#{dest_file}\\1\""] ];
+    ret.push([/"index.php\?page=#{page}"/, dest_sub]) if subpage == "index";
+    ret.push([/"index.php"/,               dest_sub]) if    page == "home";
+    ret;
+  end.flatten(1)
+  
+  page_data = 
+    sources.map do |page, subpage, dest_file|
+      IO.popen("php", "w+") do |php|
+        php.puts "<? ";
+        php.puts "$_GET['page'] = '#{page}';";
+        php.puts "$_GET['subpage'] = '#{subpage}';";
+        php.puts "?>";
+        php.puts(File.open(File.basename script) { |f| f.readlines.join(""); });
+        php.close_write;
+        script_data = php.readlines.join("");
+        subs.each { |pat,subst| script_data.gsub!(pat, subst); }
+        [ dest_file, script_data ];
+      end
+    end
+  
+  Dir.chdir old_dir;
+  
+  page_data.each do |dest_file, script_data|
+    File.open("#{target_dir}/#{dest_file}", "w") { |f| f.puts script_data }
+  end
+end
+
+copy_files(["../doc/README", 
+            "../doc/LICENSE"], "dbtoaster");
 
 copy_files(["../bin/dbtoaster"], "dbtoaster/bin");
 copy_files([], "dbtoaster/lib");
+
+copy_files(["../doc/site/9.jpg",
+            "../doc/site/style.css",
+            "../doc/site/bakeoff.png",
+            "../doc/site/dbtoaster-logo.gif"], "dbtoaster/doc");
+build_php("../doc/site/index.php", "dbtoaster/doc");
 
 copy_files(Dir.glob("../lib/dbt_c++/*.hpp"), "dbtoaster/lib/dbt_c++");
 copy_files(Dir.glob("../lib/dbt_c++/*.cpp"), "dbtoaster/lib/dbt_c++");
@@ -61,6 +119,9 @@ copy_files(["../lib/dbt_scala/dbtlib.jar",
             "../lib/dbt_scala/src"], "dbtoaster/lib/dbt_scala");
 
 copy_files([], "dbtoaster/examples");
+copy_files(["../lib/dbt_c++/main.cpp",
+            "../lib/dbt_scala/src/org/dbtoaster/RunQuery.scala"], 
+                          "dbtoaster/examples/code");
 copy_files([], "dbtoaster/examples/queries");
 copy_files([], "dbtoaster/examples/data");
 copy_files(Dir.glob("../../../experiments/data/simple/tiny/*.dat"), 
