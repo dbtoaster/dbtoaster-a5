08d6689570476097297f7ed1ee120fa64b8fac56
Christoph Koch <christoph.koch@epfl.ch>
2012-07-10 09:19:48 +0000

    a version of missedtrades that lets m3 explode
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@2545 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 1f2ebb62acf2f9ddf19c87977a0aa206cf067f18

---
+++ test/queries/finance/chrissedtrades.sql
@@ -0,0 +1,33 @@
+CREATE STREAM bids(t FLOAT, id INT, broker_id INT, volume FLOAT, price FLOAT)
+  FROM FILE '../../experiments/data/finance/tiny/vwap.csv'
+  LINE DELIMITED orderbook (book := 'bids', brokers := '10', 
+                            deterministic := 'yes');
+
+CREATE STREAM asks(t FLOAT, id INT, broker_id INT, volume FLOAT, price FLOAT)
+  FROM FILE '../../experiments/data/finance/tiny/vwap.csv'
+  LINE DELIMITED orderbook (book := 'asks', brokers := '10', 
+                            deterministic := 'yes');
+
+
+
+SELECT b.broker_id, sum(a.foo + b.foo) AS mst
+FROM (SELECT b0.broker_id, sum(-1 * b0.price * b0.volume * ba.cnt) AS foo
+      FROM bids b0,
+           (SELECT count(*) AS cnt FROM asks) ba
+      WHERE 0.25*(SELECT sum(b1.volume) FROM bids b1) >
+                 (SELECT sum(b2.volume) FROM bids b2
+                  WHERE b2.price > b0.price)
+      GROUP BY b0.broker_id
+     ) b,
+     (SELECT ab.broker_id, sum(aa.foo) AS foo
+      FROM (SELECT sum(a0.price * a0.volume) AS foo
+            FROM asks a0
+            WHERE 0.25*(SELECT sum(a1.volume) FROM asks a1) >
+                       (SELECT sum(a2.volume) FROM asks a2
+                        WHERE a2.price > a0.price)
+           ) aa,
+           bids ab
+      GROUP BY ab.broker_id
+     ) a
+WHERE a.broker_id = b.broker_id
+GROUP BY b.broker_id
