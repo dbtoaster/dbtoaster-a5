1420297a506ed4778dec28d5daa06d3e6a3b6c63
Oliver Kennedy <okennedy@buffalo.edu>
2013-10-11 19:36:45 +0000

    Minor doc tweaks, committing test case
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@3058 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 2bbf265a7f30d05be5f29cb00a2f08d7855cc196

---
+++ test/queries/function/cos.sql
@@ -0,0 +1,10 @@
+CREATE STREAM RadialMeasurements ( angle double, distance double )
+  FROM FILE '../../experiments/data/simple/standard/r.dat' LINE DELIMITED CSV;
+
+
+CREATE FUNCTION cos ( x double ) RETURNS int AS EXTERNAL 'cos';
+CREATE FUNCTION sin ( x double ) RETURNS int AS EXTERNAL 'sin';
+
+SELECT r.distance * cos(r.angle) AS x, 
+       r.distance * sin(r.angle) AS y
+FROM RadialMeasurements r;
