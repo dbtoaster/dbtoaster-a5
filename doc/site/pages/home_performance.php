ed2d05bb1367e2b01d6de7eb2946a7b7f5676763
Andres Noetzli <noetzli@dbtoaster.org>
2014-05-24 01:06:27 +0000

    d3 graph on website for bakeoff
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@3239 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 5b6a9367d225e7ec99e7c0b0c969c6c59dc52632

--- doc/site/pages/home_performance.php
+++ doc/site/pages/home_performance.php
@@ -1,5 +1,88 @@
-<img src="engine_bakeoff0.png" class="inline_image" width="600" />
-<img src="engine_bakeoff1.png" class="inline_image" width="600" />
+<script src="http://d3js.org/d3.v3.min.js"></script>
+
+<div class="bakeoff"></div>
+<script>
+
+var margin = {top: 20, right: 20, bottom: 70, left: 40},
+    width = 600 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
+
+var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+
+var y = d3.scale.linear().range([height, 0]);
+
+var xAxis = d3.svg.axis()
+   .scale(x)
+   .orient("bottom");
+
+var yAxis = d3.svg.axis()
+   .scale(y)
+   .orient("left")
+   .ticks(10);
+
+var svg = d3.select(".bakeoff").append("svg")
+      .attr("height", height + margin.top + margin.bottom)
+   .append("g")
+      .attr("transform",
+         "translate(" + margin.left + "," + margin.top + ")");
+
+d3.csv("data/bakeoff.csv", function(error, data) {
+   x.domain(data.map(function(d) { return d.query; }));
+   y.domain([0, d3.max(data, function(d) { return Math.max(d.cpp, d.scala, d.scalalms); })]);
+
+   svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+   .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", "-.55em")
+      .attr("transform", "rotate(-90)" );
+
+   svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+   .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Execution time (s)");
+
+   var groups = ["scala", "scalalms", "cpp"];
+   var barwidth = x.rangeBand() / (groups.length + 1)
+
+   svg.selectAll("bar")
+      .data(data)
+   .enter().append("rect")
+      .attr("class", "barscala")
+      .attr("x", function(d) { return x(d.query) + 0.5 * barwidth; })
+      .attr("width", barwidth)
+      .attr("y", function(d) { return y(d.scala); })
+      .attr("height", function(d) { return height - y(d.scala); });
+
+   svg.selectAll("bar")
+      .data(data)
+   .enter().append("rect")
+      .attr("class", "barscalalms")
+      .attr("x", function(d) { return x(d.query) + 1.5 * barwidth; })
+      .attr("width", barwidth)
+      .attr("y", function(d) { return y(d.scalalms); })
+      .attr("height", function(d) { return height - y(d.scalalms); });
+
+   svg.selectAll("bar")
+      .data(data)
+   .enter().append("rect")
+      .attr("class", "barcpp")
+      .attr("x", function(d) { return x(d.query) + 2.5 * barwidth; })
+      .attr("width", barwidth)
+      .attr("y", function(d) { return y(d.cpp); })
+      .attr("height", function(d) { return height - y(d.cpp); });
+});
+
+</script>
+
 
 <p>The above graphs show a performance comparison of DBToaster-generated query engines against a commercial database system (DBX), a commercial stream processor (SPY), and several naive query evaluation strategies implemented in DBToaster that do not involve the Higher-Order incremental view maintenance. (The commercial systems remain anonymous in accordance with the terms of their licensing agreements).</p>
 
