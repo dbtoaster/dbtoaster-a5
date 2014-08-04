e196e7d07daba26af060e19c79728c24ef3051f6
Milos Nikolic <Milos.Nikolic@epfl.ch>
2014-08-04 17:41:55 +0000

    installation page updated
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@3285 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 3c5587b5d58bcf3b8f3d9ce1700c91db3224cb9f

--- doc/site/pages/docs.php
+++ doc/site/pages/docs.php
@@ -1,20 +1,20 @@
 <p>
-The DBToaster compiler can be installed on Windows, Mac OS X, and Linux platforms. The compiler supports generation of C++ and Scala programs. Depending on the chosen backend(s), it requires the following dependencies:
+The DBToaster compiler can be installed on Windows, Mac OS X, and Linux platforms. The compiler supports generation of C++ and Scala programs and,  depending on the chosen backend(s), requires the following dependencies:
 <ul>
-  <li>Oracle JDK 7 (or above), required</li>
+  <li>Oracle JDK 7 (or above) - required</li>
   <li>g++ 4.8 (or above) - optional, for compiling generated C++ programs</li>
   <li><a href="http://scala-lang.org/download/2.10.2.html">Scala 2.10.2</a> - optional, for compiling generated Scala programs</li>
-  <li><a href="http://cygwin.com/">Cygwin</a> (only for Windows)</li>
+  <li><a href="http://cygwin.com/">Cygwin</a> - required only for Windows, see the below video on how to install DBToaster on Windows</li>
 </ul>
 
 <i>Notes:</i>
 <ul>
   <li>Most of the development and testing has been done on Mac OS X and Linux platforms.</li>
-  <li>To use a custom C++ compiler, create a symlink <tt>gpp</tt> in the DBToaster directory that points to the compiler (e.g., <span class="code"> "ln -s /usr/local/bin/g++-4.8 gpp"</span>).</li>
   <li>Other versions of the tools/packages might work as well but have not been tested.</li>
+  <li>To use a custom C++ compiler, create a symlink <tt>gpp</tt> in the DBToaster directory that points to the compiler (e.g., <span class="code"> "ln -s /usr/local/bin/g++-4.8 gpp"</span>).</li>
 </ul>
 
-Visit <?= mk_link(null, "download", null); ?> to get the binaries.
+Visit <?= mk_link(null, "download", null); ?> to get the latest binaries for different platforms. Each binary comes with a set of example SQL queries and  datasets to get you started. 
 </p>
 
 <p align="center">
