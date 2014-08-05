b5a37f4ecb47f3ae384e4a2797a0b3dcd0662c25
Mohammad Dashti <Mohammad.Dashti@epfl.ch>
2014-08-05 13:32:08 +0000

    Added a nice download button to the website homepage!
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@3306 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: e366977d073abfe9219626f753c92177b9db3cc8

--- doc/site/pages/home.php
+++ doc/site/pages/home.php
@@ -1,3 +1,33 @@
+<?php if(!$now_building_distro) { ?>
+<div class="container">
+  <section class="btn-wrapper-download">
+    <div class="download-btns">
+      <span class="download-text"><i class="fa fa-cloud-download">&nbsp;&nbsp;</i>Download DBToaster 2</span>
+      <div class="linux-btn-wrapper" style="display: none;">
+        <div class="linux-bit" style="display: none;"> <!-- -->
+          <ul>
+            <li><a href="dist/dbtoaster_rhel6.5_x86_64_3263.tgz">&nbsp;&nbsp;&nbsp;RHEL&nbsp;&nbsp;&nbsp;</a></li>
+            <li><a href="dist/dbtoaster_ubuntu12.04_x86_64_3263.tgz">&nbsp;Ubuntu&nbsp;</a></li>
+          </ul>
+        </div>
+        <a class="linux-btn" title="Choose your Linux System" ><i class="fa fa-linux"></i></a>
+      </div>
+      <a href="dist/dbtoaster_darwin_3263.tgz" class="mac-btn" title="Mac OS X Lion (10.6.8)" style="display: none;"><i class="fa fa-apple"></i></a>
+      <a href="dist/dbtoaster_cygwin_3263.tgz" class="windows-btn" title="Windows (Cygwin)" style="display: none;"><i class="fa fa-windows"></i></a>
+    </div>
+    <!--span id="download-info" style="display: none;">Completely free 7-day full trial</span-->
+  </section>
+  
+  <!-- Store Button -->
+  <!--section class="btn-wrapper-shop">
+    <div class="store-btn">
+      <a href="javascript:void(0)">Store</a>
+    </div>
+    <span id="shop-info" style="display: none;">Individual license: 49 US$</span> 
+  </section-->
+</div>
+<?php } ?>
+
 <p>DBToaster is an SQL-to-native-code compiler. It generates lightweight, specialized, embeddable query engines for applications that require real-time, low-latency data processing and monitoring capabilities.
 The DBToaster compiler generates code that can be easily incorporated into any C++ or JVM-based (Java, Scala, ...) project.
 </p>
