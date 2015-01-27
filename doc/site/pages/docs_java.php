a91d82f3595a8e84c8329b70a2dfbbe3a8fe95e7
Mohammad Dashti <Mohammad.Dashti@epfl.ch>
2015-01-27 10:27:05 +0000

    Synced Scala documentation with Java documentation + Generated static HTML pages for the documentation
    git-svn-id: file:///Users/ckoch/repos/assembla.com/boo@3363 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 34fa822ddff0ac8eb054bb16ce8b4c9da72b1a64

--- doc/site/pages/docs_java.php
+++ doc/site/pages/docs_java.php
@@ -67,20 +67,20 @@ class ExampleApp {
 
       // Send events
       Tuple2 result;
-      System.out.println("Insert a tuple to R.");
+      System.out.println("Insert a tuple into R.");
       q.tell(new TupleEvent(0, TUPLE_INSERT, "R", tuple(5L, 2L)), null);
 
-      System.out.println("Insert a tuple to S.");
+      System.out.println("Insert a tuple into S.");
       q.tell(new TupleEvent(1, TUPLE_INSERT, "S", tuple(2L, 3L)), null);
 
-      System.out.println("Insert a tuple to T.");
+      System.out.println("Insert a tuple into T.");
       q.tell(new TupleEvent(2, TUPLE_INSERT, "T", tuple(3L, 4L)), null);
 
       inbox.send(q, new GetSnapshot(new $colon$colon(1, EMPTY_LIST)));
       result = (Tuple2) (inbox.receive(DEFAULT_TIMEOUT));
       System.out.println("Result after this step: " + result._2().toString());
 
-      System.out.println("Insert another tuple to T.");
+      System.out.println("Insert another tuple into T.");
       q.tell(new TupleEvent(3, TUPLE_INSERT, "T", tuple(3L, 5L)), null);
 
       // Retrieve result
@@ -155,10 +155,10 @@ $> jar cvf exampleapp.jar ExampleApp.class
 
 <div class="codeblock">
 $> java -classpath "rst.jar:lib/dbt_scala/*:exampleapp.jar" ExampleApp
-Insert a tuple to R.
-Insert a tuple to S.
-Insert a tuple to T.
+Insert a tuple into R.
+Insert a tuple into S.
+Insert a tuple into T.
 Result after this step: List(20)
-Insert another tuple to T.
+Insert another tuple into T.
 Final Result: List(45)
 </div>
