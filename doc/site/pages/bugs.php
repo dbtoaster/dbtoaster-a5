24afe2d9066875241fbb41df6a6c858601fac528
christophkoch <christoph.koch@epfl.ch>
2017-09-22 12:11:20 +0200

    website folder moved out of compiler/alpha5

--- doc/site/pages/bugs.php
+++
@@ -1,8 +0,0 @@
-<iframe src="http://www.dbtoaster.org/bugs/"
-        align='center' 
-        width='100%' 
-        height='500px' 
-        scrolling="auto"
-        name='mantis' 
-        onload='parent.scrollTo(0,0);'>
-</iframe>
