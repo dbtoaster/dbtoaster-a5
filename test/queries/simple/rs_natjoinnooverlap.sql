823db325dfb70983e3f08bb6cf33ec7aeefa0f1d
Milos Nikolic <milos.nikolic@ed.ac.uk>
2020-11-10 10:55:01 +0000

    fixing a wrong source path

--- test/queries/simple/rs_natjoinnooverlap.sql
+++ test/queries/simple/rs_natjoinnooverlap.sql
@@ -3,7 +3,7 @@ FROM FILE '../../experiments/data/simple/tiny/r.dat' LINE DELIMITED
 CSV ();
 
 CREATE STREAM S(C int, D int)
-FROM FILE '../../experiments/data/simple/tiny/r.dat' LINE DELIMITED
+FROM FILE '../../experiments/data/simple/tiny/s.dat' LINE DELIMITED
 CSV ();
 
 SELECT R.* FROM R NATURAL JOIN S;
